5d2dbd2a4567fb0cdec4fda2b29f033cbb2cb374
bsharpataz <bsharp@email.arizona.edu>
2019-01-19 15:02:16 -0700

    output dir and flag for keeping tars

--- equation_extraction/expand_arxiv.py
+++ equation_extraction/expand_arxiv.py
@@ -6,6 +6,8 @@ import subprocess
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('dirname') # the path with the arxiv arXiv_src_*_*.tar files
+    parser.add_argument('outputdir')  # the path with the arxiv arXiv_src_*_*.tar files
+    parser.add_argument('--keepall', default=False)
     args = parser.parse_args()
     return args
 
@@ -27,14 +29,15 @@ def gunzip_file(f):
 # make a directory for the paper file(s), return path
 def mk_paper_dir(f):
     paper_dir = f + "_dir"
-    try:
-        os.mkdir(paper_dir)
-    except:
-        print "mkdir error"
+    if not os.path.exists(paper_dir):
+        os.makedirs(paper_dir)
     return paper_dir
 
 # wdir: the path with the arxiv arXiv_src_*_*.tar files
-def expand_arxiv(wdir):
+def expand_arxiv(wdir, outdir, keep_all):
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+
     arxiv_files = glob.glob(os.path.join(wdir, "*.tar"))
     print "arXiv dump files to be expanded:", arxiv_files
 
@@ -44,13 +47,13 @@ def expand_arxiv(wdir):
         assert(tarfile.is_tarfile(fn) == True) # for good measure...
         tar = tarfile.open(fn)
         extracted_files = tar.getnames()
-        tar.extractall(path=wdir)
+        tar.extractall(path=outdir)
         tar.close()
 
         counter = 0
         for f in extracted_files:
             # path to the extracted file that needs to be handled
-            f = os.path.join(wdir, f)
+            f = os.path.join(outdir, f)
 
             # We expect that this file will be a gzipped file or a pdf
             # If it's a gzipped file (i.e., with latex source):
@@ -68,6 +71,10 @@ def expand_arxiv(wdir):
                     paper_tar = tarfile.open(base_path)
                     paper_tar.extractall(path=paper_dir)
                     paper_tar.close()
+                    # Remove the tar file
+                    if not keep_all:
+                        subprocess.check_call(['rm', base_path])
+
                 # Case 2: it's the tex file missing its extension
                 else:
                     # rename it and move to the paper dir
@@ -77,13 +84,17 @@ def expand_arxiv(wdir):
 
             # Otherwise, if it's a pdf, disregard
             elif is_pdf(f):
-                pass
+                if not keep_all:
+                    subprocess.check_call(['rm', f])
             # There are a few other formats, we also will disregard them, but just in case we want to see
             # how many and of what type...
             else:
                 print "INFO: didn't handle file ", f
+                # not removing here bc we get the main dir in the list, I guess bc of the api for
+                # tarfile.getnames()...
+
         print "... finished extracting {0} papers".format(counter)
 
 if __name__ == '__main__':
     args = parse_args()
-    expand_arxiv(args.dirname)
+    expand_arxiv(args.dirname, args.outputdir, args.keepall)
