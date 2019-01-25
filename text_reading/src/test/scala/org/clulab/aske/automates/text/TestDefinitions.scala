77edbf764a816ae32615879205ba5a9019d6ab2c
bsharpataz <bsharp@email.arizona.edu>
2019-01-25 14:52:38 -0700

    added second half of tests

--- text_reading/src/test/scala/org/clulab/aske/automates/text/TestDefinitions.scala
+++ text_reading/src/test/scala/org/clulab/aske/automates/text/TestDefinitions.scala
@@ -78,37 +78,118 @@ class TestDefinitions extends ExtractionTest {
     val mentions = extractMentions(t6)
     testDefinitionEvent(mentions, desired)
   }
+
   // Tests from paper: 2016-Camargo-and Kemanian-Six-crop-models differ-in-their-simulation-of water-uptake
 
   val t7 = "In APSIM, water uptake (Ta, mm d−1) is determined from potential transpiration demand (Tp, mm d−1), soil " +
     "water available (WA, mm d−1), and water supply (WS, mm d−1) for each ith day and soil layer as:"
+  passingTest should "find definitions from t7" in {
+    val desired = Map(
+      "Ta" -> Seq("water uptake"),
+      "Tp" -> Seq("potential transpiration demand"),
+      "WA" -> Seq("soil water available"),
+      "WS" -> Seq("water supply")
+    )
+    val mentions = extractMentions(t7)
+    testDefinitionEvent(mentions, desired)
+  }
 
   val t8 = "where fi is the daily fractional light interception, ETo is the daily reference evapotranspiration (mm d−1)," +
     " \\\"pwp is the water content at permanent wilting point (m3 m−3), $z is the soil layer thickness (m), and kl " +
     "is the water extraction rate, an empiric soil–root factor for the fraction of available water that can be " +
     "supplied to the plant from each rooted soil layer."
+  passingTest should "find definitions from t9" in {
+    val desired = Map(
+      "fi" -> Seq("daily fractional light interception"),
+      "ETo" -> Seq("daily reference evapotranspiration"),
+      "pwp" -> Seq("water content at permanent wilting point"),
+      "$z" -> Seq("soil layer thickness"),
+      "kl" -> Seq("water extraction rate", "empiric soil–root factor for the fraction of available water that can " +
+        "be supplied to the plant from each rooted soil layer")
+    )
+    val mentions = extractMentions(t8)
+    testDefinitionEvent(mentions, desired)
+  }
 
   val t9 = "This means that kl represents a maximum supply determined by !r and the resistance to water flow " +
     "(Passioura, 1983; Monteith, 1986)"
+  passingTest should "find definitions from t9" in {
+    val desired = Map(
+      "kl" -> Seq("maximum supply determined by !r and the resistance to water flow")
+    )
+    val mentions = extractMentions(t9)
+    testDefinitionEvent(mentions, desired)
+  }
 
   val t10 = "The plant conductance is calculated by inverting the transpiration equation using a maximum expected " +
     "transpiration (Tx, mm d−1), the soil water potential at field capacity ( Sfc, J kg−1) and the leaf water " +
     "potential at the onset of stomatal closure ( Lsc, J kg−1):"
+  passingTest should "find definitions from t10" in {
+    val desired = Map(
+      //"plant conductance" -> Seq("inverting the transpiration equation"), // todo: do we want this one?
+      "Tx" -> Seq("maximum expected transpiration"),
+      "Sfc" -> Seq("soil water potential at field capacity"),
+      "Lsc" -> Seq("leaf water potential at the onset of stomatal closure")
+    )
+    val mentions = extractMentions(t10)
+    testDefinitionEvent(mentions, desired)
+  }
 
   val t11 = "The average soil water potential ( ̄S , J kg−1) is calculated based on a representative root length " +
     "fraction for each soil layer (fr,j):"
+  passingTest should "find definitions from t11" in {
+    val desired = Map(
+      "S" -> Seq("average soil water potential"),
+      "fr,j" -> Seq("representative root length fraction for each soil layer")
+    )
+    val mentions = extractMentions(t11)
+    testDefinitionEvent(mentions, desired)
+  }
 
   val t12 = "The L is calculated using T, Cp and  ̄S ."
+  passingTest should "find NO definitions from t12" in {
+    val desired = Map.empty[String, Seq[String]]
+    val mentions = extractMentions(t12)
+    testDefinitionEvent(mentions, desired)
+  }
 
   val t13 = "If L falls below that of permanent wilting point ( Lpwp), then Ta = 0"
+  passingTest should "find definitions from t13" in {
+    val desired = Map(
+      "Lpwp" -> Seq("permanent wilting point") // todo: or "that of permanent wilting point" ??
+    )
+    val mentions = extractMentions(t13)
+    testDefinitionEvent(mentions, desired)
+  }
 
   val t14 = "Finally, Ta is calculated using s and L, Cp and Tp:"
+  passingTest should "find NO definitions from t14" in {
+    val desired =  Map.empty[String, Seq[String]]
+    val mentions = extractMentions(t14)
+    testDefinitionEvent(mentions, desired)
+  }
 
   val t15 = "For this research Tx = 10 mm d−1, Lsc = −1100 J kg−1 and Lpwp = −2000 J kg−1."
+  passingTest should "find NO definitions from t15" in {
+    val desired =  Map.empty[String, Seq[String]]
+    val mentions = extractMentions(t15)
+    testDefinitionEvent(mentions, desired)
+  }
 
   val t16 = "In DSSAT, root water uptake is calculated in two steps."
+  passingTest should "find NO definitions from t16" in {
+    val desired =  Map.empty[String, Seq[String]]
+    val mentions = extractMentions(t16)
+    testDefinitionEvent(mentions, desired)
+  }
 
   val t17 = "First, water uptake per unit of root length is computed in each soil layer (Url, m3 m−1 d−1) as an " +
     "exponential function that depends on:"
-
+  passingTest should "find definitions from t17" in {
+    val desired = Map(
+      "Url" -> Seq("water uptake per unit of root length") // todo: is this right ??
+    )
+    val mentions = extractMentions(t17)
+    testDefinitionEvent(mentions, desired)
+  }
 }
