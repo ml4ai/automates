a80c82cc6d731c9136ded55b163bf826f238d1e2
Paul Hein <pauldhein@clara>
2019-01-26 09:09:47 -0800

    much better word embeddings

--- code_summarization/w2v.sh
+++ code_summarization/w2v.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 W2VDIR=/work/pauldhein/trunk/
-IN=./data/corpus/comm-sentences.output
+IN=/work/pauldhein/code-corpus/corpus/code-sentences-full.output
 
-time $W2VDIR/word2vec -train $IN -output comm-vectors.txt -cbow 0 -size 50 -window 5 -negative 0 -hs 1 -sample 1e-3 -threads 12 -binary 0
+time $W2VDIR/word2vec -train $IN -output code-vectors.txt -cbow 0 -size 50 -window 5 -negative 0 -hs 1 -sample 1e-3 -threads 12 -binary 0
 
 # $W2VDIR/distance vectors.txt.bin
