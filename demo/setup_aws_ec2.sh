c105c5a7481645dfc790624e2b6b076464026f1e
Adarsh Pyarelal <adarsh@email.arizona.edu>
2019-02-07 18:35:15 +0000

    Updating instructions with Anaconda Python - incomplete still

--- demo/setup_aws_ec2.sh
+++ demo/setup_aws_ec2.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # This set of commands sets up the AutoMATES demo on a fresh AWS EC2 instance
 
 # Symlink automates demo
@@ -6,24 +7,28 @@ sudo ln -sfT ~/automates/demo /var/www/html/automates_demo
 
 # Update package lists
 sudo apt-get update
+cd
+wget https://repo.anaconda.com/archive/Anaconda3-2018.12-Linux-x86_64.sh
+bash Anaconda3-2018.12-Linux-x86_64.sh
+
+conda create -n automates
+source activate automates
+pip install -r requirements.txt
 
 # Install required packages using apt
 sudo apt-get install \
   graphviz\
   libgraphviz-dev\
   pkg-config\
-  python3-pip\
   apache2\
+  apache2-dev\
   libapache2-mod-wsgi-py3\
   default-jre\
-  python3-flask
 
-# Install virtualenv
-sudo pip3 install virtualenv
+
 
 # Clone repos
 git clone https://github.com/ml4ai/delphi
+mv delphi ../../
 sudo ln -sfT ~/delphi /var/www/html/delphi
-sudo virtualenv venv
-. venv/bin/activate
-sudo pip3 install -r requirements.txt
+
