c908552fa7fee0f425dabee961bdd1779ba7947e
bsharpataz <bsharp@email.arizona.edu>
2019-02-13 15:08:39 -0700

    update path

--- equation_extraction/docker.sh
+++ equation_extraction/docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-DATA=$1
 IMAGE=clulab/equations
-docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$DATA":/data "$IMAGE" "$@"
+DATADIR=/projects/automates # hard coded for venti
+# DATADIR=$PWD # hard coded for laptop
+docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$DATADIR":/data "$IMAGE" "$@"
