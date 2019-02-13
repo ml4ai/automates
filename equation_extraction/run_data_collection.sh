ea1f4e73669baa2a93bb965b926a77d6e481f359
bsharpataz <bsharp@email.arizona.edu>
2019-02-13 11:34:53 -0700

    cmd line args

--- equation_extraction/run_data_collection.sh
+++ equation_extraction/run_data_collection.sh
@@ -1,49 +1,71 @@
 #!/bin/bash
 
+# we need at least the --indir option
+if [ $# == 0 ]; then
+    echo "please specify at least the input dir"
+    exit 1
+fi
+
 # read options
-OPTIONS=$(getopt ... -- "$@")
-eval set -- "$OPTIONS"
+OPTIONS=$(getopt -o i:o:l:t:r:dkp:n: -l indir:,outdir:,logfile:,template:,rescale-factor:,dump-pages,keep-intermediate-files,pdfdir:,nproc: -- "$@")
+
+# report errors
+if [ $? != 0 ]; then
+    echo "Terminating ..." >&2
+    exit $?
+fi
+
+# set parsed options
+eval set -- $OPTIONS
+
+# default values
+ARGS=""
+NPROC=1
 
 # extract options and their arguments
 while true; do
     case "$1" in
         -i|--indir)
+            INDIR=$2
+            shift 2
             ;;
         -o|--outdir)
+            ARGS="$ARGS --outdir=$2"
+            shift 2
             ;;
         -l|--logfile)
+            ARGS="$ARGS --logfile=$2"
+            shift 2
             ;;
         -t|--template)
+            ARGS="$ARGS --template=$2"
+            shift 2
             ;;
         -r|--rescale-factor)
+            ARGS="$ARGS --rescale-factor=$2"
+            shift 2
             ;;
         -d|--dump-pages)
+            ARGS="$ARGS --dump-pages"
+            shift
             ;;
         -k|--keep-intermediate-files)
+            ARGS="$ARGS --keep-intermediate-files"
+            shift
             ;;
         -p|--pdfdir)
+            ARGS="$ARGS --pdfdir=$2"
+            shift 2
+            ;;
+        -n|--nproc)
+            NPROC=$2
+            shift 2
             ;;
         --)
-            shift
             break
-            ;;
-        *)
-            echo "ERROR"
-            exit 1
             ;;
     esac
 done
 
-INPUTDIR=$1
-OUTPUTDIR=$2
-#logfile
-#template
-# rescale-factor
-# dump-pages
-#keep-intermediate-files
-# pdfdir
-PDFDIR=$3
-NPROCS=$3
-
 # read papers
-find $DATADIR -type d -regextype posix-awk -regex '.*/[0-9]{4}\.[0-9]{5}' -print0 | xargs -0 -I{} -n 1 -P $NPROCS python -u /equation_extraction/collect_data.py {}
+find $DATADIR -type d -regextype posix-awk -regex '.*/[0-9]{4}\.[0-9]{5}' -print0 | xargs -0 -I{} -n 1 -P $NPROC python -u /equation_extraction/collect_data.py {} $ARGS
