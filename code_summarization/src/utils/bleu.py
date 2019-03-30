dbd75bcef3a7895e14174c1e93f4225961bce750
Paul Hein <pauldhein@email.arizona.edu>
2019-03-30 11:35:51 -0700

    Additional output stats

--- code_summarization/src/utils/bleu.py
+++ code_summarization/src/utils/bleu.py
@@ -1,19 +1,34 @@
 import sys
 import numpy as np
 from nltk.translate.bleu_score import sentence_bleu
+from utils import CODE_CORPUS
+
 
 references = list()
-with open(sys.argv[1], "r") as reference_file:
+ref_path = CODE_CORPUS / "results" / sys.argv[1]
+with open(ref_path, "r") as reference_file:
     for line in reference_file.readlines():
         references.append(line.strip().split(" "))
 
 translations = list()
-with open(sys.argv[2], "r") as translation_file:
+trans_path = CODE_CORPUS / "results" / sys.argv[2]
+with open(trans_path, "r") as translation_file:
     for line in translation_file.readlines():
         translations.append(line.strip().split(" "))
 
 scores = list()
 for reference, translation in zip(references, translations):
-    scores.append(sentence_bleu([reference], translation, weights=(0.5, 0.5)))
+    score = sentence_bleu([reference], translation, weights=(1.0,))
+    scores.append((score, reference, translation))
 
-print(f"Average BLEU score: {np.mean(scores)}")
+nums = [s for s, _, _ in scores]
+print(f"Average BLEU score: {np.mean(nums)}")
+
+scores.sort(key=lambda tup: tup[0], reverse=True)
+for i, (score, ref, trans) in enumerate(scores[:100]):
+    print(f"RANK: {i}\tSCORE: {score}")
+    print(f"\tTRUTH: {ref}\n")
+    print(f"\tTRANS: {trans}\n")
+    print("\n")
+
+print(sorted(nums, reverse=True)[:500])
