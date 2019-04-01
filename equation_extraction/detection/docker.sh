1393c94398ca7308abfcc75d84657758aef539e9
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2019-04-01 11:04:03 -0700

    changes

--- equation_extraction/detection/docker.sh
+++ equation_extraction/detection/docker.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-IMAGE=maskrcnn-cpu
+IMAGE=maskrcnn-gpu
 DATA=$PWD
-# docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$DATA":/data "$IMAGE" "$@"
-docker run -p 8888:8888 -v "$DATA":/data "$IMAGE" jupyter notebook --allow-root
+docker run --runtime=nvidia --rm -i --user="$(id -u):$(id -g)" -v "$DATA":/data "$IMAGE" "$@"
