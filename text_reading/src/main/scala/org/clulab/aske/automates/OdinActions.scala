91e89257aa0130b5c652faa0776e0b0e32071bf1
Masha Alexeeva <maxaalexeeva@gmail.com>
2019-05-14 20:42:00 -0700

    adjustments to rules and such to help the aligner

--- text_reading/src/main/scala/org/clulab/aske/automates/OdinActions.scala
+++ text_reading/src/main/scala/org/clulab/aske/automates/OdinActions.scala
@@ -133,7 +133,7 @@ class OdinActions(val taxonomy: Taxonomy, expansionHandler: Option[ExpansionHand
     }
       if words.length == 1
       word = m.words.head
-      if word.length <= 5
+      if word.length <= 6
       if word.toLowerCase != word // mixed case or all UPPER
     } yield m
   }
