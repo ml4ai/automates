dd07a343411c587d981a8f6e2c680da861f5021f
bsharpataz <bsharp@email.arizona.edu>
2019-08-26 14:42:45 -0700

    revert docker.sh

--- equation_extraction/docker.sh
+++ equation_extraction/docker.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 IMAGE=clulab/equations
-#DATADIR=/projects/automates # hard coded for venti
-DATADIR=$PWD # hard coded for laptop
-# DATADIR=/Users/bsharp/github/automates/equation_extraction
+DATADIR=/projects/automates # hard coded for venti
+# DATADIR=$PWD # hard coded for laptop
 docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$DATADIR":/data "$IMAGE" "$@"
