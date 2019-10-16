0402e7806f0403d7b16ee439e88417a223ab5c49
bsharpataz <bsharp@email.arizona.edu>
2019-10-16 12:28:03 -0700

    export shortcut

--- pdfalign/pdfalign.py
+++ pdfalign/pdfalign.py
@@ -736,6 +736,7 @@ class PdfAlign(Frame):
         self.bind_all("u", lambda e: self.unit_rb.invoke())
         self.bind_all("s", lambda e: self.save_annotation())
         self.bind_all("k", lambda e: self.toggle_boxes())
+        self.bind_all("x", lambda e: self.export_annotations())
         self.bind_all("<Tab>", lambda e: self.next_mode())
 
         viewer = Frame(self)
