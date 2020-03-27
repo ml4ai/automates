7ce598a6d0229f23ee65ab2d02d5bcac0fbc7476
Paul Hein <pauldhein@email.arizona.edu>
2020-03-26 21:04:41 -0400

    Adding a beginning script to test ER pipeline

---
+++ scripts/model_analysis/er_integration.py
@@ -0,0 +1,20 @@
+import requests
+
+
+def main():
+    webservice = "http://localhost:8000"
+
+    eqn_pic_path = "/Users/phein/Google Drive/ASKE-AutoMATES/Data/Mini-SPAM/eqns/SPAM/PET/PETPT/8.png"
+    res = requests.post(
+        f"{webservice}/decode_equation/",
+        headers={"Content-type": "multipart/form-data"},
+        data={"file": open(eqn_pic_path, "rb"), "type": "image/png"},
+    )
+
+    data = res.json()
+    print(data)
+    print(type(data))
+
+
+if __name__ == "__main__":
+    main()
