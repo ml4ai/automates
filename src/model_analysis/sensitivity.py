9c2a255a2d5d14a9ee6196e90388d62ecd980cc4
skhan <souratosh.khan86@gmail.com>
2020-03-27 12:45:44 -0700

    adding sensitivity code with correct path

--- src/model_analysis/sensitivity.py
+++ src/model_analysis/sensitivity.py
@@ -4,8 +4,8 @@ from SALib.sample import saltelli
 from SALib.analyze import sobol
 import numpy as np
 import json
-from delphi.GrFN.networks import ComputationalGraph
-from delphi.GrFN.utils import timeit
+from model_analysis.networks import ComputationalGraph
+from model_analysis.utils import timeit
 
 
 class InputError(Exception):
