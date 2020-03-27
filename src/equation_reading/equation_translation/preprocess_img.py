5e2d984320362f419dac184739ff43553cb03e5a
bsharpataz <bsharp@email.arizona.edu>
2020-03-26 17:34:04 -0700

    added script to preprocess eqn imgs

---
+++ src/equation_reading/equation_translation/preprocess_img.py
@@ -0,0 +1,16 @@
+from PIL import Image
+import numpy as np
+import png
+import sys
+
+def reformat(fn, fn_out):
+    grey = np.array(Image.open(fn).convert('L'))
+    grey[grey>175] = 255
+    grey_pil = Image.fromarray(grey)
+    img = grey_pil.point(lambda x: int(x/17))
+    #(w, h) = img.size
+    png.fromarray(np.asarray(img, np.uint8),'L;4').save(fn_out)
+
+if __name__ == "__main__":
+    args = sys.argv
+    reformat(args[1], args[2])
