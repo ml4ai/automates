77ca82a09fc46d8fcd9c953e0b4148dae0695e16
Paul Hein <pauldhein@email.arizona.edu>
2020-03-28 12:49:22 -0400

    Updated paths for other users

--- scripts/model_analysis/tr_integration.py
+++ scripts/model_analysis/tr_integration.py
@@ -1,14 +1,13 @@
 import requests
 import json
+import os
 
 webservice = "http://localhost:9000"
 
-# TODO: update this to no longer depend on my google drive path
-mini_spam = "/Users/phein/Google Drive/ASKE-AutoMATES/Data/Mini-SPAM"
-cur_dir = "/Users/phein/repos/aske/automates/scripts/model_analysis"
-
-pet_docs = f"{mini_spam}/docs/SPAM/PET"
-pet_eqns = f"{mini_spam}/eqns/SPAM/PET"
+cur_dir = os.getcwd()
+automates_data = os.environ["AUTOMATES_DATA"]
+pet_docs = f"{automates_data}/Mini-SPAM/docs/SPAM/PET"
+pet_eqns = f"{automates_data}/Mini-SPAM/eqns/SPAM/PET"
 
 
 def test_pdf_to_mentions():
@@ -25,14 +24,13 @@ def test_pdf_to_mentions():
 
 def test_align():
     # NOTE: relies upon test_pdf_to_mentions being run previously
-    petpt_grfn = json.load(open("PETPT_GrFN.json", "r"))
     res = requests.post(
         f"{webservice}/align",
         headers={"Content-type": "application/json"},
         json={
             "mentions": f"{cur_dir}/PT-mentions.json",
             "equations": f"{pet_eqns}/PETPT/PETPT_equations.txt",
-            "grfn": json.dumps(petpt_grfn),
+            "grfn": f"{cur_dir}/PETPT_GrFN.json",
         },
     )
     print(res)
