cdc94474f8a4b95015a3da0866b8ec1f9a6fe6d8
Paul Hein <pauldhein@email.arizona.edu>
2020-03-27 22:40:08 -0400

    Fixed my er integration script

--- scripts/model_analysis/er_integration.py
+++ scripts/model_analysis/er_integration.py
@@ -7,8 +7,8 @@ def main():
     eqn_pic_path = "8.png"
     res = requests.post(
         f"{webservice}/decode_equation/",
-        headers={"Content-type": "multipart/form-data"},
-        data={"file": open(eqn_pic_path, "rb"), "type": "image/png"},
+        # headers={"Content-type": "multipart/form-data"},
+        files={"file": (eqn_pic_path, open(eqn_pic_path, "rb"), "image/png")},
     )
 
     data = res.json()
