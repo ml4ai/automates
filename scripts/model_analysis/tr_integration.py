d38383e6d3450131bd9cf00c0837f4ac98af4c4f
Maria <maxaalexeeva@gmail.com>
2020-03-29 03:06:12 -0700

    restored Paul's version of tr_integration file

--- scripts/model_analysis/tr_integration.py
+++ scripts/model_analysis/tr_integration.py
@@ -5,42 +5,34 @@ import os
 webservice = "http://localhost:9000"
 
 cur_dir = os.getcwd()
-# automates_data = os.environ["AUTOMATES_DATA"]
-# pet_docs = f"{automates_data}/Mini-SPAM/docs/SPAM/PET"
-# pet_eqns = f"{automates_data}/Mini-SPAM/eqns/SPAM/PET"
+automates_data = os.environ["AUTOMATES_DATA"]
+pet_docs = f"{automates_data}/Mini-SPAM/docs/SPAM/PET"
+pet_eqns = f"{automates_data}/Mini-SPAM/eqns/SPAM/PET"
 
 
 def test_pdf_to_mentions():
     res = requests.post(
         f"{webservice}/pdf_to_mentions",
         headers={"Content-type": "application/json"},
-        json={
-            "pdf": f"{pet_docs}/petpt_2012.pdf",
-            "outfile": f"{cur_dir}/PT-mentions.json",
-        },
+        json={"pdf": f"{pet_docs}/petpt_2012.pdf"},
     )
     print(res)
-    # mentions = res.json()
+    mentions = res.json()
     # mentions = json_dict["mentions"]
-    # json.dump(mentions, open("PT-mentions.json", "w"))
+    json.dump(mentions, open("PT-mentions.json", "w"))
 
 
 def test_align():
     # NOTE: relies upon test_pdf_to_mentions being run previously
-    # mentions = json.load(open("PT-mentions.json", "r"))
-    # mentions["outfile"] = f"{cur_dir}/PT-alignment.json"
-    # json.dump(mentions, open("PT-mentions.json", "w"))
-
     res = requests.post(
         f"{webservice}/align",
         headers={"Content-type": "application/json"},
         json={
-            "mentions": "/home/alexeeva/Downloads/PT-mentions.json",
-            "equations": "/home/alexeeva/Repos/automates/scripts/model_analysis/PETPT_equations.txt",
-            "grfn": "/home/alexeeva/Repos/automates/scripts/model_analysis/PETPT_GrFN.json",
+            "mentions": f"{cur_dir}/PT-mentions.json",
+            "equations": f"{pet_eqns}/PETPT/PETPT_equations.txt",
+            "grfn": f"{cur_dir}/PETPT_GrFN.json",
         },
     )
-    print("Done")
     print(res)
     json_dict = res.json()
     json.dump(json_dict, open("PT-alignment.json", "w"))
