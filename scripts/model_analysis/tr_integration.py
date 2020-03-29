4c90df93ba0f5739d45ff28cf81878c44628e951
Maria <maxaalexeeva@gmail.com>
2020-03-29 09:54:28 -0700

    restoring Pauls tr_integration file for real this time

--- scripts/model_analysis/tr_integration.py
+++ scripts/model_analysis/tr_integration.py
@@ -14,16 +14,23 @@ def test_pdf_to_mentions():
     res = requests.post(
         f"{webservice}/pdf_to_mentions",
         headers={"Content-type": "application/json"},
-        json={"pdf": f"{pet_docs}/petpt_2012.pdf"},
+        json={
+            "pdf": f"{pet_docs}/petpt_2012.pdf",
+            "outfile": f"{cur_dir}/PT-mentions.json",
+        },
     )
     print(res)
-    mentions = res.json()
+    # mentions = res.json()
     # mentions = json_dict["mentions"]
-    json.dump(mentions, open("PT-mentions.json", "w"))
+    # json.dump(mentions, open("PT-mentions.json", "w"))
 
 
 def test_align():
     # NOTE: relies upon test_pdf_to_mentions being run previously
+    # mentions = json.load(open("PT-mentions.json", "r"))
+    # mentions["outfile"] = f"{cur_dir}/PT-alignment.json"
+    # json.dump(mentions, open("PT-mentions.json", "w"))
+
     res = requests.post(
         f"{webservice}/align",
         headers={"Content-type": "application/json"},
