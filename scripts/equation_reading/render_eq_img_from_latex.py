c9218abddc9d0644be0de65b0f7e9b27e1669410
Clayton Morrison <claytonm@email.arizona.edu>
2020-03-29 01:18:33 -0700

    refactored, separating general utility

--- scripts/equation_reading/render_eq_img_from_latex.py
+++ scripts/equation_reading/render_eq_img_from_latex.py
@@ -6,7 +6,7 @@
 # -----------------------------------------------------------------------------
 
 import os
-import subprocess
+import src.equation_reading.equation_extraction.render_image_from_latex as rifl
 
 
 # -----------------------------------------------------------------------------
@@ -16,98 +16,22 @@ import subprocess
 # NOTE: Must be updated to your local!
 ASKE_GOOGLE_DRIVE_ROOT = '/Users/claytonm/Google Drive/ASKE-AutoMATES'
 
-EQN_SOURCE_ROOT = os.path.join(ASKE_GOOGLE_DRIVE_ROOT, 'Data/Mini-SPAM/eqns/SPAM/PET')
-PETPT_ROOT = os.path.join(EQN_SOURCE_ROOT, 'PETPT')
+MODEL_ROOT = os.path.join(ASKE_GOOGLE_DRIVE_ROOT, 'Data/Mini-SPAM/eqns/SPAM/PET')
+PETPT_ROOT = os.path.join(MODEL_ROOT, 'PETPT')
 
 
 # -----------------------------------------------------------------------------
 # Render image from latex
 # -----------------------------------------------------------------------------
 
-def standalone_eq_template(eqn):
-    template = '\\documentclass{standalone}\n' \
-               '\\usepackage{amsmath}\n' \
-               '\\usepackage{amssymb}\n' \
-               '\\begin{document}\n' \
-               f'$\\displaystyle {{{{ {eqn} }}}} $\n' \
-               '\\end{document}'
-    return template
-
-
-def render_image_from_latex(latex_src, eqn_tex_dst_root, image_dst_root, verbose=False, test_p=True):
-    """
-
-    Args:
-        latex_src: filepath to file containing single latex equation on each line
-        eqn_tex_dst_root: root path for individual latex .tex files (1 per eqn)
-        image_dst_root: root path for generated .png files
-        verbose:
-        test_p: flag for whether to run in test mode (don't actually generate anything)
-
-    Returns:
-
-    """
-
-    eqn_tex_dst_root = os.path.abspath(eqn_tex_dst_root)
-
-    if not test_p:
-        if not os.path.exists(eqn_tex_dst_root):
-            os.makedirs(eqn_tex_dst_root)
-
-    image_dst_root = os.path.abspath(image_dst_root)
-
-    if not test_p:
-        if not os.path.exists(image_dst_root):
-            os.makedirs(image_dst_root)
-
-    if verbose:
-        print(f'latex_src: {latex_src}')
-        print(f'eqn_tex_dst_root: {eqn_tex_dst_root}')
-        print(f'image_dst_root: {image_dst_root}')
-
-    with open(latex_src, 'r') as fin:
-        for i, line in enumerate(fin.readlines()):
-            eqn_latex = line.strip('\n')
-            latex_output = standalone_eq_template(eqn_latex)
-            if verbose:
-                print('-'*20, i)
-
-            eqn_tex_file = os.path.join(eqn_tex_dst_root, f'{i}.tex')
-
-            if verbose:
-                print(f'writing {eqn_tex_file}')
-            if not test_p:
-                with open(eqn_tex_file, 'w') as fout:
-                    fout.write(latex_output)
-
-            command_args = ['pdflatex', '-output-directory', eqn_tex_dst_root, eqn_tex_file]
-
-            if verbose:
-                print(f'suprocess.run({command_args})')
-            if not test_p:
-                subprocess.run(command_args)
-                if verbose:
-                    print('    after subprocess.run')
-                # cleanup
-                os.remove(os.path.join(eqn_tex_dst_root, f'{i}.aux'))
-                os.remove(os.path.join(eqn_tex_dst_root, f'{i}.log'))
-
-            eqn_pdf_file = os.path.join(eqn_tex_dst_root, f'{i}.pdf')
-            eqn_png_file = os.path.join(image_dst_root, f'{i}.png')
-
-            command_args = ['convert',
-                            '-background', 'white', '-alpha',
-                            'remove', '-alpha', 'off',
-                            '-density', '200',
-                            '-quality', '100',
-                            f'{eqn_pdf_file}', f'{eqn_png_file}']
-
-            if verbose:
-                print(f'suprocess.run({command_args})')
-            if not test_p:
-                subprocess.run(command_args)
-                if verbose:
-                    print('    after subprocess.run')
+def render_images_for_model(model_root, model_name, verbose, test_p):
+    model_latex_source = os.path.join(model_root, f'{model_name}_equations.txt')
+    model_eqn_tex_dst_root = os.path.join(os.path.join(model_root, 'manual_latex'), 'tex')
+    model_image_dst_root = os.path.join(model_root, 'manual_eqn_images')
+    rifl.render_image_from_latex(model_latex_source,
+                                 model_eqn_tex_dst_root,
+                                 model_image_dst_root,
+                                 verbose=verbose, test_p=test_p)
 
 
 # -----------------------------------------------------------------------------
@@ -116,6 +40,10 @@ def render_image_from_latex(latex_src, eqn_tex_dst_root, image_dst_root, verbose
 
 if __name__ == '__main__':
     # TODO: provide proper cli interface
+
+    render_images_for_model(PETPT_ROOT, 'PETPT', verbose=True, test_p=False)
+
+    '''
     PETPT_LATEX_SOURCE = os.path.join(PETPT_ROOT, 'PETPT_equations.txt')
     PETPT_EQN_TEX_DST_ROOT = os.path.join(os.path.join(PETPT_ROOT, 'manual_latex'), 'tex')
     PETPT_IMAGE_DST_ROOT = os.path.join(PETPT_ROOT, 'manual_eqn_images')
@@ -124,3 +52,4 @@ if __name__ == '__main__':
                             PETPT_IMAGE_DST_ROOT,
                             verbose=True,
                             test_p=False)
+    '''
