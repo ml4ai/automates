aca256ac69b14ac8e4e6ab3f60b44edf0bf19bc3
Paul Hein <pauldhein@email.arizona.edu>
2020-03-30 18:09:54 -0400

    Still having trouble getting variables

--- scripts/tex2py.py
+++ scripts/tex2py.py
@@ -1,7 +1,24 @@
 import sys
 import re
+import os
 
 from sympy.parsing.latex import parse_latex
+
+
+RESERVED_WORDS = {
+    "max",
+    "min",
+    "frac",
+    "exp",
+    "sin",
+    "cos",
+    "tan",
+    "arctan",
+    "arccos",
+    "arcsin",
+    "ln",
+}
+
 
 def latex2python(symbols_str):
     try:
@@ -18,19 +35,30 @@ def sanitize_token_tex(tokenized_tex):
         r"\\left|\\right|\\mathrm\{Int\}", "", tokenized_tex
     )
     tokenized_tex = re.sub(r"~", " ", tokenized_tex)
-    vars = re.findall(r"[a-z]+\^[a-z]|(?!\\)[a-z]+_\{[a-z]+\}", tokenized_tex)
+    words = re.findall(
+        r"[A-Za-z]+\^[A-Za-z]|[A-Za-z]+_[A-Za-z0-9]|[A-Za-z]+_\{[A-Za-z0-9]+\}|[A-Za-z]+",
+        tokenized_tex,
+    )
+    print(words)
+    unique_words = set(words)
+    vars = list(unique_words - RESERVED_WORDS)
     print(vars)
     return tokenized_tex
 
 
 if __name__ == "__main__":
-    eqn_filepath = sys.argv[1]
+    automates_data = os.environ["AUTOMATES_DATA"]
+    mini_spam_eqns = f"{automates_data}/Mini-SPAM/eqns/SPAM/PET"
+    eqn_filepath = f"{mini_spam_eqns}/PETASCE/PETASCE_equations.txt"
     equations = list()
     with open(eqn_filepath, "r") as infile:
-        for eqn_tex in infile:
+        for l, eqn_tex in enumerate(infile):
+            if l == 5:
+                break
             no_ws_eqn_tex = eqn_tex.strip()
             if no_ws_eqn_tex == "None":
                 continue
+            print(no_ws_eqn_tex)
             sanitized_eqn_tex = sanitize_token_tex(no_ws_eqn_tex)
             python_eqn = latex2python(sanitized_eqn_tex)
             equations.append(
