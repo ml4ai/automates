4e982ba518f7348808bd10666322d93d2d9967e9
Paul Hein <pauldhein@email.arizona.edu>
2020-04-17 13:43:16 -0700

    Removing the old link table script

--- scripts/model_analysis/pet_link_tables.py
+++
@@ -1,9 +0,0 @@
-import json
-import sys
-
-import model_analysis.linking as linking
-
-
-grfn = json.load(open(sys.argv[1], "r"))
-tables = linking.make_link_tables(grfn)
-linking.print_table_data(tables)
