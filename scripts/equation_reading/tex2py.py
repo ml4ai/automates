c11629d163690257507f599d054309ed26cd14d7
Paul Hein <pauldhein@email.arizona.edu>
2020-04-17 10:32:52 -0700

    Reformatting script to output translated equations and print results line by line

--- scripts/equation_reading/tex2py.py
+++ scripts/equation_reading/tex2py.py
@@ -1,31 +1,40 @@
-import re
-import os
+"""
+This program translates all tokenized LaTeX equations from an input text file
+into syntactically equivalent executable python expressions.
+
+
+Example execution:
+    $ python tex2py.py <path-to-equations.txt> <path/to/py_equations.txt>
+
+Author: Paul D. Hein, Marco Valenzuela
+"""
+
 import sys
 
-# insert path to decoding.py
-sys.path.insert(0, os.path.abspath("src/equation_reading"))
+from equation_reading.decoding import tex2py
 
-from decoding import tex2py
 
+def main():
+    eqn_filepath = sys.argv[1]
+    py_equations = list()
+    with open(eqn_filepath, "r") as infile:
+        for tex_eqn_line in infile:
+            no_ws_tex_eqn_line = tex_eqn_line.strip()
+            if no_ws_tex_eqn_line == "None":
+                continue
+            eqn_data = tex2py(no_ws_tex_eqn_line)
+
+            print(f"ORIGINAL:\t\t{eqn_data['original']}")
+            print(f"SANITIZED:\t\t{eqn_data['sanitized']}")
+            print(f"TRANSLATED:\t\t{eqn_data['translated']}")
+            print("\n")
+            py_equations.append(eqn_data["translated"])
+
+    py_eqn_filepath = sys.argv[2]
+    with open(py_eqn_filepath, "w") as outfile:
+        for py_eqn in py_equations:
+            outfile.write(py_eqn)
 
 
 if __name__ == "__main__":
-
-    automates_data = os.environ["AUTOMATES_DATA"]
-    mini_spam_eqns = f"{automates_data}/Mini-SPAM/eqns/SPAM/PET"
-    eqn_filepath = f"{mini_spam_eqns}/PETASCE/PETASCE_equations.txt"
-    equations = list()
-    with open(eqn_filepath, "r") as infile:
-        for l, eqn_tex in enumerate(infile):
-            no_ws_eqn_tex = eqn_tex.strip()
-            if no_ws_eqn_tex == "None":
-                continue
-            if l == 5:
-                break
-            equations.append(tex2py(no_ws_eqn_tex))
-
-    for eqns in equations:
-        print(f"ORIGINAL:\t\t{eqns['original']}")
-        print(f"SANITIZED:\t\t{eqns['sanitized']}")
-        print(f"TRANSLATED:\t\t{eqns['translated']}")
-        print("\n")
+    main()
