3704cfecdcfd4609f33ce8d298547d7ff4dcdb02
Paul Hein <pauldhein@email.arizona.edu>
2020-04-27 15:08:59 -0700

    Improving ISA MaxS1 trees

--- scripts/model_analysis/test_isa.py
+++ scripts/model_analysis/test_isa.py
@@ -1,3 +1,5 @@
+import networkx as nx
+
 from model_analysis.sensitivity import ISA, SensitivityAnalyzer
 from model_analysis.networks import GroundedFunctionNetwork
 
@@ -12,7 +14,10 @@ B = {
     "msalb": [0.0, 1.0],
     "xhlai": [0.0, 20.0],
 }
-N = 10000
-analyzer = SensitivityAnalyzer.Si_from_Sobol
-res = ISA(G, B, N, analyzer)
-print(res)
+N = 1000
+analyzer = SensitivityAnalyzer.Si_from_RBD_FAST
+V = ISA(G, B, N, analyzer, max_iterations=7)
+V.graph["graph"] = {"rankdir": "LR"}
+V.graph["edges"] = {"arrowsize": "4.0"}
+A = nx.nx_agraph.to_agraph(V)
+A.draw("isa-graph.pdf", prog="dot")
