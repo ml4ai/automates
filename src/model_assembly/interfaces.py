6a19d57a8aa227944badd10a54ea4d97317bdf68
Paul D. Hein <pauldhein@email.arizona.edu>
2020-05-28 12:48:02 -0400

    Adding a model asssembly text interface

--- src/model_assembly/interfaces.py
+++ src/model_assembly/interfaces.py
@@ -24,7 +24,7 @@ class TextReadingInterface:
         return json.load(open(out_path, "r"))
 
     def get_link_hypotheses(
-        mentions_path: str, eqns_path: str, air_path: str
+        self, mentions_path: str, eqns_path: str, air_path: str
     ) -> dict:
         if not os.path.isfile(mentions_path):
             raise RuntimeError(f"Mentions not found: {mentions_path}")
@@ -32,22 +32,67 @@ class TextReadingInterface:
         if not os.path.isfile(eqns_path):
             raise RuntimeError(f"Equations not found: {eqns_path}")
 
-        grfn_path = f"{pet_grfns}/{grfn_name}.json"
-        if not os.path.isfile(grfn_path):
-            raise RuntimeError(f"GrFN not found: {grfn_name}")
+        if not os.path.isfile(air_path):
+            raise RuntimeError(f"AIR not found: {air_path}")
+
+        if not mentions_path.endswith(".json"):
+            raise ValueError("/align expects mentions to be a JSON file")
+
+        if not eqns_path.endswith(".txt"):
+            raise ValueError("/align expects equations to be a text file")
+
+        if not air_path.endswith(".json"):
+            raise ValueError("/align expects AIR to be a JSON file")
+
+        air_data = json.load(open(air_path, "r"))
+        air_variables = list(
+            set(
+                [
+                    {"name": "::".join(v["name"].split("::")[:-1]) + "::0"}
+                    for v in air_data["variables"]
+                ]
+            )
+        )
+        payload = {
+            "mentions": mentions_path,
+            "equations": eqns_path,
+            "source_code": {
+                "variables": air_variables,
+                "comments": air_data["source_comments"],
+            },
+            "toggles": {"groundToSVO": False, "appendToGrFN": False},
+            "arguments": {"maxSVOgroundingsPerVar": 5},
+        }
+        payload_path = "align_payload.json"
+        json.dump(payload, open("payload_path", "w"))
 
         res = requests.post(
-            f"{webservice}/align",
+            f"{self.webservice}/align",
             headers={"Content-type": "application/json"},
-            json={
-                "mentions": mentions_path,
-                "equations": eqns_path,
-                "grfn": grfn_path,
-            },
+            json={"pathToJson": payload_path},
         )
-        print(f"HTTP {res} for /align on ({mentions_name}, {grfn_name})")
+        print(f"HTTP {res} for /align on:\n\t{mentions_path}\n\t{air_path}\n")
         json_dict = res.json()
-        json.dump(json_dict, open(f"{out_name}.json", "w"))
+        return json_dict
+
+    def ground_to_SVO(self, mentions_path: str) -> dict:
+        if not os.path.isfile(mentions_path):
+            raise RuntimeError(f"Mentions file not found: {mentions_path}")
+
+        if not mentions_path.endswith(".json"):
+            raise ValueError(
+                "/groundMentionsToSVO expects mentions to be a JSON file"
+            )
+
+        res = requests.post(
+            f"{self.webservice}/groundMentionsToSVO",
+            headers={"Content-type": "application/json"},
+            json={"mentions": mentions_path},
+        )
+
+        print(f"HTTP {res} for /groundMentionsToSVO on {mentions_path}")
+        json_dict = res.json()
+        return json_dict
 
 
 class EquationReadingInterface:
