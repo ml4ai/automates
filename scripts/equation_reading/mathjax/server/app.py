3e05eb76ab1fd0186740753a093566ffc6fcbd7d
Paul D. Hein <pauldhein@email.arizona.edu>
2020-07-08 14:14:56 -0400

    Adding a controller and fixing index

--- scripts/equation_reading/mathjax/server/app.py
+++ scripts/equation_reading/mathjax/server/app.py
@@ -1,50 +1,39 @@
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, render_template, jsonify, session
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    print('Calling index()')
-    return render_template('index.html')
-    '''
-    try:
-        print('Calling index()')
-        ctx = render_template('index.html')
-        print(ctx)
-        return ctx
-    except Exception as e:
-        print(f'ERROR index(): {str(e)}')
-        return str(e)
-    '''
+    return render_template("index.html")
 
 
 # then end '/' is necessary...
-@app.route('/interactive/')
+@app.route("/interactive")
 def interactive():
     try:
-        print('Calling interactive()')
-        ctx = render_template('interactive.html')
-        print('    SUCCESS render_template() - ctx:')
+        print("Calling interactive()")
+        ctx = render_template("interactive.html")
+        print("    SUCCESS render_template() - ctx:")
         print(ctx)
         return ctx
     except Exception as e:
-        print(f'ERROR interactive(): {str(e)}')
+        print(f"ERROR interactive(): {str(e)}")
         return str(e)
 
 
-@app.route('/background_process')
+@app.route("/background_process")
 def background_process():
     try:
-        print('Calling background_process()')
-        lang = request.args.get('proglang', 0, type=str)
-        if lang.lower() == 'python':
-            return jsonify(result='You are wise.')
+        print("Calling background_process()")
+        lang = request.args.get("proglang", 0, type=str)
+        if lang.lower() == "python":
+            return jsonify(result="You are wise.")
         else:
-            return jsonify(result='Try again.')
+            return jsonify(result="Try again.")
     except Exception as e:
         return str(e)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
