0ed6b459e12d417d494d405822bb090eb2cae5c5
Clayton Morrison <claytonm@email.arizona.edu>
2020-07-11 15:57:48 -0700

    adding second column to load eqn images, reverting back to load data from .js

--- scripts/equation_reading/mathjax/render/static/js/generate-table.js
+++ scripts/equation_reading/mathjax/render/static/js/generate-table.js
@@ -10,6 +10,8 @@ $(document).ready(function(){
   let table = $("#table");
   let i = 0;
 
+  /*
+  // DOES NOT WORK...
   // load eqn_src from json in data_path
   var eqn_src = (function() {
     var json = null;
@@ -24,16 +26,28 @@ $(document).ready(function(){
     });
     return json;
   })();
+  */
+
+  // console.log("after attempt to load");
+  console.log(eqn_src);
 
   // For each latex source datum in data:
   //   generate a table row, with
   //     <td> for original latex source (as plain text)
+  //     <td> for loading image of latex-rendered
   //     <td> that has innerHTML as raw mml -- available to be rendered by MathJax
   //     <td> that contains pre-formatted MathML
   for (let element of eqn_src) {
+
+    console.log(`in loop... ${i}`);
+
     let row = $("<tr/>",{ class: "datum" });
     var cell = $("<td/>", { id: `tex_src_${i}`,
-                            text: `${element["src"]}` });
+                            text: `${i}: ${element["src"]}` });
+    row.append(cell);
+
+    image_path = `${images_path}/${i}.${images_ext}`;
+    cell = $("<td/>", { id: `tex_img_${i}` }).append(`<img src="${image_path}" alt="${image_path}" width="200">`);
     row.append(cell);
     
     mml = `${element["mml"]}`;
@@ -42,7 +56,7 @@ $(document).ready(function(){
     row.append(cell);
     
     // xml-formatter options to display xml more compactly
-	let mml_formatted = format_xml(mml, {
+	mml_formatted = format_xml(mml, {
 	  indentation: '  ',
 	  collapseContent: true, 
 	  lineSeparator: '\n'
@@ -54,7 +68,7 @@ $(document).ready(function(){
     
     row.append(cell);
     
-    table.append(row)
+    table.append(row);
     
     i++;
   }
