d8f93a71aead28a74f7d6a190dc69eb5db8d3725
Paul D. Hein <pauldhein@email.arizona.edu>
2020-07-27 18:31:09 -0400

    DFS traversal of Python AST printing linenos of assg stmts

---
+++ scripts/program_analysis/ast_explorer.py
@@ -0,0 +1,29 @@
+import ast
+import sys
+
+
+def dfs_traversal(nodes: list):
+    for node in nodes:
+        if isinstance(node, ast.FunctionDef):
+            print(
+                f"{node.name} --> {len(node.args.args)} args\t{len(node.body)} lines"
+            )
+        elif isinstance(node, ast.Assign):
+            print(f"{node.lineno}\t{node.targets[0].id} = {node.value}")
+            return
+        else:
+            print(type(node), vars(node))
+        dfs_traversal(ast.iter_child_nodes(node))
+
+
+def main():
+    code = ""
+    with open(sys.argv[1], "r") as infile:
+        code = "".join(infile.readlines())
+
+    tree = ast.parse(code)
+    dfs_traversal(ast.iter_child_nodes(tree))
+
+
+if __name__ == "__main__":
+    main()
