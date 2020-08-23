564e0303f142d4de77192cbe817592ec2357fd95
Clayton Morrison <claytonm@email.arizona.edu>
2020-08-23 09:44:28 -0700

    adding CHIME model doc path

--- scripts/equation_reading/render_eq_img_from_latex.py
+++ scripts/equation_reading/render_eq_img_from_latex.py
@@ -35,14 +35,16 @@ import automates.equation_reading.equation_extraction.render_image_from_latex as
 # NOTE: Must be updated to your local!
 ASKE_GOOGLE_DRIVE_ROOT = parameters.get()['AUTOMATES_DATA']
 
-MODEL_ROOT = os.path.join(ASKE_GOOGLE_DRIVE_ROOT, 'Mini-SPAM/eqns/SPAM/PET')
-PETPT_ROOT = os.path.join(MODEL_ROOT, 'PETPT')
-PETASCE_ROOT = os.path.join(MODEL_ROOT, 'PETASCE')
-PETDYN_ROOT = os.path.join(MODEL_ROOT, 'PETDYN')
-PETMEY_ROOT = os.path.join(MODEL_ROOT, 'PETMEY')
-PETPEN_ROOT = os.path.join(MODEL_ROOT, 'PETPEN')
-PETPNO_ROOT = os.path.join(MODEL_ROOT, 'PETPNO')
+MODEL_ROOT_PET = os.path.join(ASKE_GOOGLE_DRIVE_ROOT, 'Mini-SPAM/eqns/SPAM/PET')
+PETPT_ROOT = os.path.join(MODEL_ROOT_PET, 'PETPT')
+PETASCE_ROOT = os.path.join(MODEL_ROOT_PET, 'PETASCE')
+PETDYN_ROOT = os.path.join(MODEL_ROOT_PET, 'PETDYN')
+PETMEY_ROOT = os.path.join(MODEL_ROOT_PET, 'PETMEY')
+PETPEN_ROOT = os.path.join(MODEL_ROOT_PET, 'PETPEN')
+PETPNO_ROOT = os.path.join(MODEL_ROOT_PET, 'PETPNO')
 
+MODEL_ROOT_COVID = os.path.join(ASKE_GOOGLE_DRIVE_ROOT, 'COVID-19')
+CHIME_ROOT = os.path.join(MODEL_ROOT_COVID, 'CHIME/eqns/2020-08-04-CHIME-docs')
 
 # -----------------------------------------------------------------------------
 # Render image from latex
@@ -72,4 +74,5 @@ if __name__ == '__main__':
     # render_images_for_model(PETDYN_ROOT, 'PETDYN', verbose=True, test_p=False)
     # render_images_for_model(PETMEY_ROOT, 'PETMEY', verbose=True, test_p=False)
     # render_images_for_model(PETPEN_ROOT, 'PETPEN', verbose=True, test_p=False)
-    # render_images_for_model(PETPNO_ROOT, 'PETPNO', verbose=True, test_p=False)x
+    # render_images_for_model(PETPNO_ROOT, 'PETPNO', verbose=True, test_p=False)
+    # render_images_for_model(CHIME_ROOT, '2020-08-04-CHIME-docs', verbose=True, test_p=False)
