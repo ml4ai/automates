52d9152978ff1f0925d8e3cfb8088edef357ece3
Gaurav Sharma <57118584+gauravsh0812@users.noreply.github.com>
2020-11-10 16:00:03 -0700

    Update MathML_Cleaner.py

--- scripts/equation_reading/mathjax/arxiv_eqn_extraction/MathML_Cleaner.py
+++ scripts/equation_reading/mathjax/arxiv_eqn_extraction/MathML_Cleaner.py
@@ -9,38 +9,44 @@ import re
 
 # Removing unnecessary information or attributes having default values 
 
-def Attribute_Definition(MMLcode, ATTRIBUTES, Attr_tobeRemoved, Attr_tobeChecked):
+def Attribute_Definition(MMLcode, ELEMENTS, Attr_tobeRemoved, Attr_tobeChecked):
     
+    # Defining array to keep Attribute definition
     Definition_array = []
     
-    for Attr in ATTRIBUTES:
+    for ele in ELEMENTS:
         
-        position = [i for i in re.finditer(r'\b%s\b' % re.escape(Attr), MML)]
+        # Getting indices of the position of the element in the MML code
+        position = [i for i in re.finditer(r'\b%s\b' % re.escape(ele), MML)]
         
         for p in position:
 
+            # Attribute begining and ending indices
             (Attr_begin, Attr_end) = p.span()
+            
+            # Length of the definition of the attribute
             Length = MMLcode[Attr_end:].find(">")   
         
             if Length >0:
-            
-                Definition = MMLcode[Attr_end: Attr_end+Length]
-                #print(Definition)
-            
-                #AttributeParameter = Definition.replace(" ", "").split("=")[0]
-                #AttributeValue= Definition.replace(" ", "").split("=")[1]
                 
-                #Definition_dict[AttributeParameter] = AttributeValue
+                # Grabbing definition
+                Definition = MMLcode[Attr_end: Attr_end+Length]
+                print("MathML element:  ", ele)
+                print("Defnition:  ", Definition)
+                
+                # Append unique definition
                 if Definition not in Definition_array:
                     Definition_array.append(Definition)
     
     
     for Darr in Definition_array:
-    
+        
+        # Attribute and its value -- of the element 
         AttributeParameter = Darr.replace(" ", "").split("=")[0]
         AttributeValue = Darr.replace(" ", "").split("=")[1]
         
-        # To be removed
+        # If Attribute has a defualt value, we can remove it
+        # Checking which attributes can be removed
         if AttributeParameter not in Attr_tobeRemoved:
             
             if AttributeParameter in Attr_tobeChecked.keys():
@@ -48,15 +54,11 @@ def Attribute_Definition(MMLcode, ATTRIBUTES, Attr_tobeRemoved, Attr_tobeChecked
                 if AttributeValue.replace('\\','').replace('"', '') == Attr_tobeChecked[AttributeParameter]:
                     
                     MMLcode = MMLcode.replace(Darr, '')         
-                    #print(MMLcode)
-              #      Definition_array.remove(Darr)
-            
-            #else:
-             #   Definition_array.remove(Darr)
-     
         else:
             MMLcode = MMLcode.replace(Darr, '')         
-            #print(MMLcode)
+    
+    
+    # Replacing greek symbols with their unicodes
     
     code_dict = {}
     
@@ -74,20 +76,22 @@ def Attribute_Definition(MMLcode, ATTRIBUTES, Attr_tobeRemoved, Attr_tobeChecked
         
         MMLcode = MMLcode.replace(str_to_replace, replacing_str)
     
+    # Printing final MML code
+    print('MathML code:  ',MMLcode)
+    
     return MMLcode
-
-
-
+    
+    
 if __name__ == '__main__':
 
     arr = open(r'C:\Users\gaura\OneDrive\Desktop\AutoMATES_local\REPO\mathml_data_dev.js', "r").readlines()
 
     for i, MML in enumerate(arr):
-        #MML=arr[4]    
+        
+        # Printing original MML
+        print("===============================")
         print('Original: \n')
         print(MML)
-        print("===============================")
-       # print(i)
         
         # Removing multiple backslashes
         i = MML.find('\\\\')
@@ -106,7 +110,7 @@ if __name__ == '__main__':
         # ATTRIBUTES
         
         ## Attributes commonly used in MathML codes to represent equations
-        ATTRIBUTES = ['mrow', 'mi', 'mn', 'mo', 'ms', 'mtext', 'math', 'mtable', 'mspace', 'maction', 'menclose', 
+        ELEMENTS = ['mrow', 'mi', 'mn', 'mo', 'ms', 'mtext', 'math', 'mtable', 'mspace', 'maction', 'menclose', 
                       'merror', 'mfenced', 'mfrac', 'mglyph', 'mlabeledtr', 'mmultiscripts', 'mover', 'mroot',
                       'mpadded', 'mphantom', 'msqrt', 'mstyle', 'msub', 'msubsup', 'msup', 'mtd', 'mtr', 'munder',
                       'munderover', 'semantics']
@@ -126,7 +130,7 @@ if __name__ == '__main__':
                            }
         
                                        
-        MML = Attribute_Definition(MML, ATTRIBUTES, Attr_tobeRemoved, Attr_tobeChecked)    
+        MML = Attribute_Definition(MML, ELEMENTS, Attr_tobeRemoved, Attr_tobeChecked)    
          
         print("Modified: \n")
         print(MML)   
