8b367421fa1420d7fbb59ccbed49dd5fe8ce0968
Maria <maxaalexeeva@gmail.com>
2021-01-06 11:41:10 -0700

    use generic paths in conf; added a comment re word embeddings file; collapse conditionals

--- automates/text_reading/src/main/scala/org/clulab/aske/automates/OdinActions.scala
+++ automates/text_reading/src/main/scala/org/clulab/aske/automates/OdinActions.scala
@@ -142,7 +142,7 @@ class OdinActions(val taxonomy: Taxonomy, expansionHandler: Option[ExpansionHand
       // If the variable was found with a Gazetteer passed through the webservice, keep it
       if ((v matches OdinEngine.VARIABLE_GAZETTEER_LABEL) && isArg) return true
       if (v.words.length == 1 && !(v.words.head.count(_.isLetter) > 0)) return false
-      if ((v.words.length > 1 || v.words.length == 1) && v.entities.get.exists(m => m matches "B-GreekLetter")) return true //account for var that include a greek letter---those are found as separate words even if there is not space
+      if ((v.words.length >= 1) && v.entities.get.exists(m => m matches "B-GreekLetter")) return true //account for var that include a greek letter---those are found as separate words even if there is not space
       if (v.words.length != 1) return false
       // Else, the variable candidate has length 1
       val word = v.words.head
