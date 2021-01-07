3d2259ac1d926ae30248b6ad92fff7fa0d9642d9
Gaurav Sharma <57118584+gauravsh0812@users.noreply.github.com>
2021-01-06 22:35:28 -0700

    Update tex_builder.py

--- scripts/equation_reading/mathjax/arxiv_eqn_extraction/tex_builder.py
+++ scripts/equation_reading/mathjax/arxiv_eqn_extraction/tex_builder.py
@@ -23,53 +23,37 @@ print('Starting at:  ', start_time)
 # Defining global lock
 lock = Lock()
 
-
-# Template for the TeX files
-def template(eqn, Preamble_DMO, Preamble_Macro):
-
-    # writing tex document for respective eqn
-    temp1 = '\\documentclass{standalone}\n' \
-               '\\usepackage{amsmath}\n' \
-               '\\usepackage{amssymb}\n'
-    temp2 = '\\begin{document}\n' \
-            f'$\\displaystyle {{{{ {eqn} }}}} $\n' \
-            '\\end{document}'
-
-    temp = temp1 + Preamble_DMO + Preamble_Macro + temp2
-    return(temp)
-
-# function to create tex documents for each eqn in the folder
-def CreateTexDoc(eqn, keyword_dict, keyword_Macro_dict, tex_folder, TeX_name):
-
-    # checking \DeclareMathOperator and Macros
-    DeclareMathOperator_in_eqn = [kw for kw in keyword_dict.keys() if kw in eqn]
-    Macros_in_eqn = [kw for kw in keyword_Macro_dict.keys() if kw in eqn]
-    Preamble_DMO, Preamble_Macro = '', ''
-    for d in DeclareMathOperator_in_eqn:
-        Preamble_DMO += "{} \n".format(keyword_dict[d])
-    for m in Macros_in_eqn:
-        Preamble_Macro += "{} \n".format(keyword_Macro_dict[m])
-
-    # creating tex file
-    path_to_tex = os.path.join(tex_folder, "{}.tex".format(TeX_name))
-    with open(path_to_tex, 'w') as f_input:
-
-        #lock.acquire()
-        f_input.write(template(eqn, Preamble_DMO, Preamble_Macro))
-        f_input.close()
-        #lock.release()
-
-def main(args_list):
-
-    #global lock 
-
-    # Unpacking argments list
-    (folder, tex_files) = args_list
+def main():
     
-    #lock.acquire()
-    #print('current folder: ', folder)
-    #lock.release()
+    year = str(args.year)
+    src_path = args.source
+      
+    for DIR in args.directories:
 
+      print('DIR:  ', DIR)
+      DIR = str(DIR)
+      
+      base_dir = os.path.join(src_path, f'{year}/{DIR}')
+
+      # Latex_equations directory
+      latex_equations = os.path.join(base_dir, "latex_equations")
+
+      # tex_files dumping directory
+      tex_files = os.path.join(base_dir, "tex_files")
+      if not os.path.exists(tex_files):
+          subprocess.call(['mkdir', tex_files])
+          
+      for folder in os.listdir(latex_equations): 
+          tex_builder([folder, tex_files, latex_equations])
+
+
+def tex_builder(args_list):
+
+    
+    # Unpacking argments list
+    (folder, tex_files, latex_equations) = args_list
+    
+    
     # creating tex folders for Large and Small equations
     tex_folder = os.path.join(tex_files, folder)
     TexFolder_Large_Eqn = os.path.join(tex_folder, "Large_eqns")
@@ -86,11 +70,8 @@ def main(args_list):
     # Dealing with "/DeclareMathOperator"
     DMO_file = os.path.join(path_to_folder, "DeclareMathOperator_paper.txt")
     with open(DMO_file, 'r') as file:
-
-        #lock.acquire()
         DMO = file.readlines()
         file.close()
-        #lock.release()
 
     # initializing /DeclareMathOperator dictionary
     keyword_dict={}
@@ -101,11 +82,8 @@ def main(args_list):
     # Dealing with "Macros"
     Macro_file = os.path.join(path_to_folder, "Macros_paper.txt")
     with open(Macro_file, 'r') as file:
-
-        #lock.acquire()
         Macro = file.readlines()
         file.close()
-        #lock.release()
 
     # initializing /Macros dictionary
     keyword_Macro_dict={}
@@ -117,64 +95,58 @@ def main(args_list):
     for Path in [LargeEqn_Path, SmallEqn_Path]:
         for File in os.listdir(Path):
 
-            #lock.acquire()
-
             main_file = os.path.join(Path, File)
             
-            #lock.acquire()
-            #print(main_file)
-            #lock.release()
-            
             with open (main_file, 'r') as FILE:
-                #lock.acquire()
                 eqn = FILE.readlines()
                 FILE.close()
-                #lock.release()
                 
             TeX_name = File.split(".")[0]
+            
             # calling function to create tex doc for the particular folder --> giving all latex eqns, DMOs, Macros and tex_folder path as arguments
             if len(eqn)!=0:
                 if Path == LargeEqn_Path:
                     CreateTexDoc(eqn[0], keyword_dict, keyword_Macro_dict, TexFolder_Large_Eqn, TeX_name)
                 else:
                     CreateTexDoc(eqn[0], keyword_dict, keyword_Macro_dict, TexFolder_Small_Eqn, TeX_name)
-            
-            #lock.release()
+
+# function to create tex documents for each eqn in the folder
+def CreateTexDoc(eqn, keyword_dict, keyword_Macro_dict, tex_folder, TeX_name):
+
+    # checking \DeclareMathOperator and Macros
+    DeclareMathOperator_in_eqn = [kw for kw in keyword_dict.keys() if kw in eqn]
+    Macros_in_eqn = [kw for kw in keyword_Macro_dict.keys() if kw in eqn]
+    Preamble_DMO, Preamble_Macro = '', ''
+    for d in DeclareMathOperator_in_eqn:
+        Preamble_DMO += "{} \n".format(keyword_dict[d])
+    for m in Macros_in_eqn:
+        Preamble_Macro += "{} \n".format(keyword_Macro_dict[m])
+
+    # creating tex file
+    path_to_tex = os.path.join(tex_folder, "{}.tex".format(TeX_name))
+    with open(path_to_tex, 'w') as f_input:
+        f_input.write(template(eqn, Preamble_DMO, Preamble_Macro))
+        f_input.close()
+        
+# Template for the TeX files
+def template(eqn, Preamble_DMO, Preamble_Macro):
+
+    # writing tex document for respective eqn
+    temp1 = '\\documentclass{standalone}\n' \
+               '\\usepackage{amsmath}\n' \
+               '\\usepackage{amssymb}\n'
+    temp2 = '\\begin{document}\n' \
+            f'$\\displaystyle {{{{ {eqn} }}}} $\n' \
+            '\\end{document}'
+
+    temp = temp1 + Preamble_DMO + Preamble_Macro + temp2
+    return(temp)
 
 
 if __name__ == "__main__":
-
-    for DIR in args.directories:
-
-      print('DIR:  ', DIR)
-      DIR = str(DIR)
-      year = str(args.year)
-
-      # paths
-      src_path = args.source
-      base_dir = os.path.join(src_path, f'{year}/{DIR}')
-
-      # Latex_equations directory
-      latex_equations = os.path.join(base_dir, "latex_equations")
-
-      # tex_files dumping directory
-      tex_files = os.path.join(base_dir, "tex_files")
-      if not os.path.exists(tex_files):
-          subprocess.call(['mkdir', tex_files])
-
-      '''
-      # loop through the folders
-      temp = []
-
-      for folder in os.listdir(latex_equations):
-
-          temp.append([folder, tex_files])
-
-      with Pool(multiprocessing.cpu_count()-12) as pool:
-          pool.map(main, temp)
-      '''
-      for folder in os.listdir(latex_equations): 
-          main([folder, tex_files])
-
-# Printing stopping time
-print('Stopping at:  ', datetime.now())
+    main()
+    
+    
+    # Printing stopping time
+    print('Stopping at:  ', datetime.now())
+    
