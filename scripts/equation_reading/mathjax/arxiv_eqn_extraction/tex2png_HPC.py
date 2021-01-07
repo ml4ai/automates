d25bd05fda4385cd6aabed72df019247d33cace5
Gaurav Sharma <57118584+gauravsh0812@users.noreply.github.com>
2021-01-06 22:45:06 -0700

    Update tex2png_HPC.py

--- scripts/equation_reading/mathjax/arxiv_eqn_extraction/tex2png_HPC.py
+++ scripts/equation_reading/mathjax/arxiv_eqn_extraction/tex2png_HPC.py
@@ -183,4 +183,4 @@ if __name__ == "__main__":
     stop_time = datetime.now()
     print('Stoping at:  ', stop_time)
     print(' ')
-    print('parsing latex equations completed.')
+    print('Tex2Png -- completed.')
