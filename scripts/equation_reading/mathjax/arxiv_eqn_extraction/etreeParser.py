a42d5d93e25f9ab9726030aa9302a956315bdcd9
Gaurav Sharma <57118584+gauravsh0812@users.noreply.github.com>
2021-01-06 17:30:47 -0700

    Update etreeParser.py

--- scripts/equation_reading/mathjax/arxiv_eqn_extraction/etreeParser.py
+++ scripts/equation_reading/mathjax/arxiv_eqn_extraction/etreeParser.py
@@ -48,89 +48,100 @@ lock = Lock()
 # Creating 'etree' directory 
 for directory in args.directories:
 
-    etree_path = f'{args.source}/{str(args.year)}/{str(directory)}/etree'
+    etreePath = f'{args.source}/{str(args.year)}/{str(directory)}/etree'
+    sample_etreePath = f'{args.source}/{str(args.year)}/{str(directory)}/sample_etree'
     
-    if not os.path.exists(etree_path):
-        subprocess.call(['mkdir', etree_path])
+    for path in [etreePath, sample_etreePath]:
+        if not os.path.exists(path):
+            subprocess.call(['mkdir', path])
         
         
 def main(DIR):    
     
     Mathml_path = f'{args.source}/{str(args.year)}/{DIR}/Simplified_mml'
     
-    args_array = pooling(Mathml_path)
+    args_array = pooling(DIR, Mathml_path)
     
     with Pool(multiprocessing.cpu_count()-10) as pool:
         result = pool.map(etree, args_array)
     
     
-def pooling(Mathml_path):
+def pooling(DIR, Mathml_path):
     
     temp = []
     
     for subDIR in os.listdir(Mathml_path):    
         
         subDIR_path = os.path.join(Mathml_path, subDIR)
-        temp.append([subDIR, subDIR_path])
+        temp.append([DIR, subDIR, subDIR_path])
     
     return temp
 
 def etree(args_array):
     
-    global etree_path
     global lock
     
     # Unpacking the args array
-    (subDIR, subDIR_path) = args_array 
+    (DIR, subDIR, subDIR_path) = args_array
     
-    for tyf in ['Large_MML', 'Small_MML']:
+    etree_path = f'{args.source}/{str(args.year)}/{DIR}/etree' 
+    sample_etree_path = f'{args.source}/{str(args.year)}/{DIR}/sample_etree' 
     
-        tyf_path = os.path.join(subDIR_path, tyf)
+    if subDIR not in os.listdir(etree_path):
         
-        # create folders
-        Create_Folders(subDIR, tyf)
+        for tyf in ['Large_MML', 'Small_MML']:
         
-        for FILE in os.listdir(tyf_path):\
+            tyf_path = os.path.join(subDIR_path, tyf)
             
-            try:
-                FILE_path = os.path.join(tyf_path, FILE)
-                
-                FILE_name = FILE.split('.')[0]
-                
-                # converting text file to xml formatted file
-                tree1 = ElementTree()
-                
-                lock.acquire()
-                #print(FILE_path)
-                lock.release()
-                
-                tree1.parse(FILE_path)
-                sample_path = f'/projects/temporary/automates/er/gaurav/sample_etree/{FILE_name}_sample.xml'
-                tree1.write(sample_path)
-                
-                # Writing etree for the xml files    
-                tree = ET.parse(sample_path)
-                xml_data = tree.getroot()
-                xmlstr = ET.tostring(xml_data, encoding='utf-8', method='xml')
-                input_string=xml.dom.minidom.parseString(xmlstr)
-                xmlstr = input_string.toprettyxml()
-                xmlstr= os.linesep.join([s for s in xmlstr.splitlines() if s.strip()]) # remove the weird newline issue
-                
-                result_path = os.path.join(etree_path, f'{subDIR}/{tyf}/{FILE_name}.xml')
-                
-                with open(result_path, "wb") as file_out:
-                    file_out.write(xmlstr.encode(sys.stdout.encoding, errors='replace'))
+            # create folders
+            Create_Folders(subDIR, tyf, etree_path, sample_etree_path)
             
-            except: logger.warning(f'{FILE_path} not working.')
+            for FILE in os.listdir(tyf_path):
                 
-def Create_Folders(subDIR, tyf):
+                try:
+                    FILE_path = os.path.join(tyf_path, FILE)
+                    
+                    FILE_name = FILE.split('.')[0]
+                    
+                    # converting text file to xml formatted file
+                    tree1 = ElementTree()
+                    tree1.parse(FILE_path)
+                    sample_path = f'{args.source}/{str(args.year)}/{DIR}/sample_etree/{subDIR}/{tyf}/{FILE_name}_sample.xml'
+                    
+                    # writing the sample files that will be used to render etree
+                    tree1.write(sample_path)
+                    
+                    
+                    # Writing etree for the xml files    
+                    tree = ET.parse(sample_path)
+                    xml_data = tree.getroot()
+                    xmlstr = ET.tostring(xml_data, encoding='utf-8', method='xml')
+                    input_string=xml.dom.minidom.parseString(xmlstr)
+                    xmlstr = input_string.toprettyxml()
+                    xmlstr= os.linesep.join([s for s in xmlstr.splitlines() if s.strip()]) 
+                    
+                    result_path = os.path.join(etree_path, f'{subDIR}/{tyf}/{FILE_name}.xml')
+                    
+                    with open(result_path, "wb") as file_out: 
+                        file_out.write(xmlstr.encode(sys.stdout.encoding, errors='replace'))
+                           
+                except:
+                    lock.acquire() 
+                    logger.warning(f'{FILE_path} not working.')
+                    lock.release()
+                    
+
+def Create_Folders(subDIR, tyf, etree_path, sample_etree_path):
     
-    global etree_path
+    global lock 
     
     etree_subDIR_path = os.path.join(etree_path, subDIR) 
     etree_tyf_path = os.path.join(etree_subDIR_path, tyf)
     
-    for F in [etree_subDIR_path, etree_tyf_path]:
+    sample_etree_subDIR_path = os.path.join(sample_etree_path, subDIR) 
+    sample_etree_tyf_path = os.path.join(sample_etree_subDIR_path, tyf)
+    
+    for F in [etree_subDIR_path, etree_tyf_path, sample_etree_subDIR_path, sample_etree_tyf_path]:
         if not os.path.exists(F):
             subprocess.call(['mkdir', F]) 
             
