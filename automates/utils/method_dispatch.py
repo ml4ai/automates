a31114430db2a17ca296e2982dd4f608a956dea9
dpdicken <dpdicken@gmail.com>
2021-01-19 11:29:53 -0700

    Add CAST visitor structure

---
+++ automates/utils/method_dispatch.py
@@ -0,0 +1,9 @@
+from functools import singledispatch, update_wrapper
+
+def methdispatch(func):
+    dispatcher = singledispatch(func)
+    def wrapper(*args, **kw):
+        return dispatcher.dispatch(args[1].__class__)(*args, **kw)
+    wrapper.register = dispatcher.register
+    update_wrapper(wrapper, func)
+    return wrapper
