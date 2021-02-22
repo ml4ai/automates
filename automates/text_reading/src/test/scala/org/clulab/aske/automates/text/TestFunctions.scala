b051316f77f30116baecae132a7abc996b326393
alicekwak <alicekwak@email.arizona.edu>
2021-02-22 11:15:58 +0900

    added function rules and example sentences
    added function rules and example sentences (to be made as tests)

---
+++ automates/text_reading/src/test/scala/org/clulab/aske/automates/text/TestFunctions.scala
@@ -0,0 +1,24 @@
+package org.clulab.aske.automates.text
+
+import org.clulab.aske.automates.TestUtils._
+
+class TestFunctions extends ExtractionTest {
+
+  // Tests from paper: ASCE-2005-The ASCE Standardized Reference-TechReport-petasce
+  // todo: "Rnl, net long-wave radiation, is the difference between upward long-wave radiation from the standardized surface (Rlu) and downward long-wave radiation from the sky (Rld),"
+ 
+  // todo: "Similar to equation 2, E0 is calculated as the product of Kcd and ETpm." (example from TestDefinitions)
+
+  // Tests from COVID_ACT_NOW
+  // todo: "Initial conditions for total cases and total exposed are calculated by dividing hospitalizations by the hospitalization rate."
+  // todo: "The default infection propagation rate ( β mild ) used in this model is an US average extracted from actual death data."
+  // todo: "Cases estimated by multiplying confirmed cases by 20."
+
+
+  // Tests from CHIME-online-manual
+  // todo: "which is the transmissibility τ multiplied by the average number of people exposed c."
+  // todo: "γ is the inverse of the mean recovery time, in days."
+  // 
+
+
+}
