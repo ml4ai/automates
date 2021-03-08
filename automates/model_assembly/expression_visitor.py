557fc0e1935d5043199519c90a9d2d27d56b913f
Paul D. Hein <pauldhein@email.arizona.edu>
2021-03-08 12:55:23 -0500

    Added documentation

--- automates/model_assembly/expression_visitor.py
+++ automates/model_assembly/expression_visitor.py
@@ -52,11 +52,7 @@ class ExprAbstractNode(ABC):
 
     @abstractmethod
     def to_dict(self):
-        """Converts the data stored in this node to a dictionary
-
-        Returns:
-            [type]: [description]
-        """
+        """Converts the data stored in this node to a dictionary"""
         pass
 
 
@@ -130,14 +126,37 @@ class ExprValueNode(ExprAbstractNode):
 
 
 class ExpressionVisitor(ast.NodeVisitor):
+    """Lambda expression walker that extends the Python AST NodeVisitor.
+
+    The purpose of this class is to create a list of ExpressionNodes from a
+    GrFN Lambda expression. These nodes include child references to other nodes
+    in the list which allows the list of nodes to fully represent a tree for
+    the lambda Expression.
+    """
+
     def __init__(self):
+        """Creates a list for the new expression nodes and stack to track UIDs
+        that are used to reference child nodes when creating parent nodes.
+        """
         self.nodes = list()
         self.uid_stack = LifoQueue()
 
-    def get_nodes(self):
+    def get_nodes(self) -> List:
+        """Return the list of ExpressionNodes that has been accumulated by a
+        call to visit().
+        """
         return self.nodes
 
     def visit_Lambda(self, node: ast.Lambda) -> NoReturn:
+        """Adds the starting position RETURN node for the root of an expression
+        tree.
+
+        This function also empties the list of nodes before beginning to
+        process a new lambda expression.
+
+        Args:
+            node (ast.Lambda): a Python AST Lambda node
+        """
         self.nodes = list()
 
         self.generic_visit(node)
@@ -147,11 +166,24 @@ class ExpressionVisitor(ast.NodeVisitor):
         self.nodes.append(new_node)
 
     def visit_Constant(self, node: ast.Constant) -> NoReturn:
+        """Adds a ValueNode as a leaf that stores some non-variable value to
+        the list of nodes.
+
+        Args:
+            node (ast.Constant): a Python AST Constant node
+        """
         new_uid = ExprAbstractNode.create_node_id()
         self.nodes.append(ExprValueNode(new_uid, node.value))
         self.uid_stack.put(new_uid)
 
     def visit_Dict(self, node: ast.Dict) -> NoReturn:
+        """Adds a Variable node with the name COMPOSITE that consists of a
+        collection of VariableNodes each with a single ValueNode child to
+        represent all elements in the given dictionary.
+
+        Args:
+            node (ast.Dict): A Python AST dictionary node
+        """
         key_uids = list()
         for key, val in zip(node.keys, node.values):
             val_uid = ExprAbstractNode.create_node_id()
@@ -171,6 +203,14 @@ class ExpressionVisitor(ast.NodeVisitor):
         self.uid_stack.put(new_uid)
 
     def visit_List(self, node: ast.List) -> NoReturn:
+        """Converts a List AST node into a LIST ExpressionNode with child nodes
+        for each element in the list.
+
+        Child nodes are ordered according to their position in the list.
+
+        Args:
+            node (ast.List): a Python AST List node
+        """
         self.generic_visit(node)
         new_uid = ExprAbstractNode.create_node_id()
         self.nodes.append(
@@ -187,6 +227,14 @@ class ExpressionVisitor(ast.NodeVisitor):
         self.uid_stack.put(new_uid)
 
     def visit_Tuple(self, node: ast.Tuple) -> NoReturn:
+        """Converts a Tuple AST node into a TUPLE ExpressionNode with child
+        nodes for each element in the tuple.
+
+        Child nodes are ordered according to their position in the tuple.
+
+        Args:
+            node (ast.Tuple): a Python AST Tuple node
+        """
         self.generic_visit(node)
         new_uid = ExprAbstractNode.create_node_id()
         self.nodes.append(
@@ -203,11 +251,21 @@ class ExpressionVisitor(ast.NodeVisitor):
         self.uid_stack.put(new_uid)
 
     def visit_Name(self, node: ast.Name) -> NoReturn:
+        """Creates a new ExprVariableNode as a leaf in the nodes list.
+
+        Args:
+            node (ast.Name): a Python AST Name node
+        """
         new_uid = ExprAbstractNode.create_node_id()
         self.nodes.append(ExprVariableNode(new_uid, node.id, []))
         self.uid_stack.put(new_uid)
 
     def visit_Subscript(self, node: ast.Subscript) -> NoReturn:
+        """Creates a new ExprVariableNode as a leaf in the nodes list.
+
+        Args:
+            node (ast.Name): a Python AST Subscript node
+        """
         new_uid = ExprAbstractNode.create_node_id()
         if isinstance(node.slice.value, ast.Constant):
             node_name = f"{node.value.id}.{node.slice.value.value}"
@@ -219,6 +277,12 @@ class ExpressionVisitor(ast.NodeVisitor):
         self.uid_stack.put(new_uid)
 
     def visit_BinOp(self, node: ast.BinOp) -> NoReturn:
+        """Creates a new ExprOperatorNode with two children for the operands
+        of this operator.
+
+        Args:
+            node (ast.BinOp): a Python AST BinOp node
+        """
         self.generic_visit(node)
         new_uid = ExprAbstractNode.create_node_id()
         self.nodes.append(
@@ -231,6 +295,12 @@ class ExpressionVisitor(ast.NodeVisitor):
         self.uid_stack.put(new_uid)
 
     def visit_UnaryOp(self, node: ast.UnaryOp) -> NoReturn:
+        """Creates a new ExprOperatorNode with one child for the operand
+        of this operator.
+
+        Args:
+            node (ast.UnaryOp): a Python AST UnaryOp node
+        """
         self.generic_visit(node)
         new_uid = ExprAbstractNode.create_node_id()
         self.nodes.append(
@@ -243,6 +313,12 @@ class ExpressionVisitor(ast.NodeVisitor):
         self.uid_stack.put(new_uid)
 
     def visit_Compare(self, node: ast.Compare) -> NoReturn:
+        """Creates a new ExprOperatorNode with n children for the n operands
+        of a comparator operator.
+
+        Args:
+            node (ast.Compare): a Python AST Compare node
+        """
         self.generic_visit(node)
         new_uid = ExprAbstractNode.create_node_id()
         comp_ops_list = [op.__class__.__name__ for op in node.ops]
@@ -264,6 +340,12 @@ class ExpressionVisitor(ast.NodeVisitor):
         self.uid_stack.put(new_uid)
 
     def visit_BoolOp(self, node: ast.BoolOp) -> NoReturn:
+        """Creates a new ExprOperatorNode with n children for the n operands
+        of a boolean operator.
+
+        Args:
+            node (ast.BoolOp): a Python AST BoolOp node
+        """
         self.generic_visit(node)
         new_uid = ExprAbstractNode.create_node_id()
         self.nodes.append(
@@ -280,6 +362,12 @@ class ExpressionVisitor(ast.NodeVisitor):
         self.uid_stack.put(new_uid)
 
     def visit_IfExp(self, node: ast.IfExp) -> NoReturn:
+        """Creates a new ExprOperatorNode with 3 children ordered [condition,
+        body, orelse] of a ternary if-expression operator.
+
+        Args:
+            node (ast.IfExp): a Python AST IfExp node
+        """
         self.generic_visit(node)
         new_uid = ExprAbstractNode.create_node_id()
         self.nodes.append(
@@ -300,6 +388,12 @@ class ExpressionVisitor(ast.NodeVisitor):
         self.uid_stack.put(new_uid)
 
     def visit_Call(self, node: ast.Call) -> NoReturn:
+        """Creates a new ExprOperatorNode with a list of children for all
+        arguments to the function call.
+
+        Args:
+            node (ast.Call): a Python AST Call node
+        """
         self.generic_visit(node)
         new_uid = ExprAbstractNode.create_node_id()
         num_args = len(node.args) + len(node.keywords)
@@ -317,6 +411,15 @@ class ExpressionVisitor(ast.NodeVisitor):
 
 
 def nodes2DiGraph(nodes: List[ExprAbstractNode]) -> nx.DiGraph:
+    """Creates a NetworkX DiGraph from a list of ExpressionNodes
+
+    Args:
+        nodes (List[ExprAbstractNode]): the list of nodes to be converted to
+                                        a DiGraph
+
+    Returns:
+        nx.DiGraph: A DiGraph that should be a tree
+    """
     uid2nodes = {n.uid: n for n in nodes}
     G = nx.DiGraph()
     G.add_nodes_from([(n, n.get_kwargs()) for n in nodes])
--- scripts/model_assembly/expression_walker.py
+++ scripts/model_assembly/expression_walker.py
@@ -1,6 +1,14 @@
+"""
+PURPOSE: Given a GrFN JSON file, this scrip generates a JSON file containing
+            expression treees for all lambda expressions found in the GrFN.
+            PDFs of each expression tree are also generated for
+            visual inspection.
+AUTHOR: Paul D. Hein
+DATE: 03/08/2020
+"""
 import ast
-import sys
 import json
+import argparse
 
 import networkx as nx
 from tqdm import tqdm
@@ -12,36 +20,47 @@ from automates.model_assembly.expression_visitor import (
 )
 
 
-def main():
-    grfn_file = sys.argv[1]
+def main(args):
+    grfn_file = args.grfn_file
     G = GroundedFunctionNetwork.from_json(grfn_file)
     visitor = ExpressionVisitor()
     func_node_graphs = list()
     for func_node in tqdm(G.lambdas, desc="Converting Lambdas"):
         node_uid = func_node.uid
         expr_tree = ast.parse(func_node.func_str)
-
         visitor.visit(expr_tree)
-        node_list = visitor.get_nodes()
-        func_network = nodes2DiGraph(node_list)
 
-        A = nx.nx_agraph.to_agraph(func_network)
-        A.graph_attr.update(
-            {"dpi": 227, "fontsize": 20, "fontname": "Menlo", "rankdir": "TB"}
-        )
-        A.node_attr.update({"fontname": "Menlo"})
-        A.draw(f"{node_uid}.pdf", prog="dot")
+        nodes = visitor.get_nodes()
+        nodes2AGraph(node_uid, nodes)
 
-        func_node_graphs.append(
-            {
-                "func_node_uid": node_uid,
-                "nodes": [n.to_dict() for n in node_list],
-            }
-        )
+        node_dicts = [n.to_dict() for n in nodes]
+        curr_func_node = {"func_node_uid": node_uid, "nodes": node_dicts}
+        func_node_graphs.append(curr_func_node)
 
     outfile_name = grfn_file.replace("--GrFN.json", "--expr-trees.json")
     json.dump(func_node_graphs, open(outfile_name, "w"))
 
 
+def nodes2AGraph(graph_name: str, nodes: list):
+    func_network = nodes2DiGraph(nodes)
+    A = nx.nx_agraph.to_agraph(func_network)
+    A.graph_attr.update(
+        {
+            "dpi": 227,
+            "fontsize": 20,
+            "fontname": "Menlo",
+            "rankdir": "TB",
+        }
+    )
+    A.node_attr.update({"fontname": "Menlo"})
+    A.draw(f"{graph_name}.pdf", prog="dot")
+
+
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        description="Convert lambda expressions in a GrFN JSON file"
+        + " into expression trees"
+    )
+    parser.add_argument("grfn_file", help="Filepath to a GrFN JSON file")
+    args = parser.parse_args()
+    main(args)
