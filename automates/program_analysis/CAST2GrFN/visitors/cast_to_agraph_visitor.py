d2d47f3d695ddb973363e7d0c867efbf41a89854
ferra <ferra@SBS-7331.local>
2021-03-16 10:53:34 -0700

    Added the rest of the CAST nodes to cast_to_pdf.py. Requires extensive testing.

--- automates/program_analysis/CAST2GrFN/visitors/cast_to_agraph_visitor.py
+++ automates/program_analysis/CAST2GrFN/visitors/cast_to_agraph_visitor.py
@@ -60,10 +60,6 @@ class CASTToAGraphVisitor(CASTVisitor):
         return None
 
     @visit.register
-    def _(self, node: Module):
-        return self.visit_list(node.body) 
-
-    @visit.register
     def _(self, node: Assignment):
         left = self.visit(node.left)
         right = self.visit(node.right)
@@ -74,31 +70,14 @@ class CASTToAGraphVisitor(CASTVisitor):
         return node_uid
 
     @visit.register
-    def _(self, node: Var):
-        val = self.visit(node.val) 
-        return val
-
-    @visit.register
-    def _(self, node: Name):
+    def _(self, node: Attribute):
+        value = node.value
+        attr = node.attr
         node_uid = uuid.uuid4()
-        self.G.add_node(node_uid,label=node.name)
-        return node_uid 
-
-    @visit.register
-    def _(self, node: Number):
-        node_uid = uuid.uuid4()
-        self.G.add_node(node_uid,label=node.number)
-        return node_uid 
-
-    @visit.register
-    def _(self, node: FunctionDef):
-        args = self.visit_list(node.func_args)
-        body = self.visit_list(node.body) 
-        node_uid = uuid.uuid4()
-        self.G.add_node(node_uid,label="Function: "+node.name.name)
-        for n in args + body:
-            self.G.add_edge(node_uid,n)
-
+        self.G.add_node(node_uid,label="Attribute")
+        self.G.add_edge(node_uid,value)
+        self.G.add_edge(node_uid,attr)
+        
         return node_uid
 
     @visit.register
@@ -113,13 +92,98 @@ class CASTToAGraphVisitor(CASTVisitor):
         return node_uid
 
     @visit.register
-    def _(self, node: UnaryOp):
-        val = self.visit(node.value)
+    def _(self, node: Call):
+        func = self.visit(node.func)
+        args = self.visit_list(node.arguments)
         node_uid = uuid.uuid4()
-        self.G.add_node(node_uid, label=node.op)
-        self.G.add_edge(node_uid, val)
+        self.G.add_node(node_uid, label="Function")
+        self.G.add_edge(node_uid, func)
+        for n in args:
+            self.G.add_edge(node_uid,n)
 
-        return node_uid        
+        return node_uid
+
+    @visit.register
+    def _(self, node: ClassDef):
+        # TODO: Where should bases field be used?
+        funcs = self.visit_list(node.funcs)
+        fields = self.visit_list(node.fields)
+        node_uid = uuid.uuid4()
+        self.G.add_node(node_uid,label="Class " + node.name)
+        for n in funcs + fields:
+            self.G.add_edge(node_uid,n)
+
+        return node_uid
+
+    @visit.register
+    def _(self, node: Dict):
+        keys = self.visit_list(node.keys)
+        values = self.visit_list(node.values)
+        node_uid = uuid.uuid4()
+        self.G.add_node(node_uid, label="Dict")
+
+        # TODO: Make this prettier
+        for n in keys + values:
+            self.G.add_edges(node_uid, n)        
+
+        #for (k,v) in list(zip(keys,values)):
+         #   self.G.add_edges(node_uid, (k,v)) 
+
+        return node_uid
+
+    @visit.register
+    def _(self, node: Expr):
+        expr = self.visit_list(node.expr)
+        node_uid = uuid.uuid4()
+        self.G.add_node(node_uid,label="Expression")
+        self.G.add_edge(node_uid,expr)
+
+        return node_uid
+
+    @visit.register
+    def _(self, node: FunctionDef):
+        args = self.visit_list(node.func_args)
+        body = self.visit_list(node.body) 
+        node_uid = uuid.uuid4()
+        self.G.add_node(node_uid,label="Function: "+node.name.name)
+        for n in args + body:
+            self.G.add_edge(node_uid,n)
+
+        return node_uid
+
+    @visit.register
+    def _(self, node: List):
+        values = self.visit_list(node.values)
+        node_uid = uuid.uuid4()
+        self.G.add_node(node_uid,label="List")
+        for n in values:
+            self.G.add_edge(node_uid,n)
+
+        return node_uid
+
+    @visit.register 
+    def _(self, node: Loop):
+        expr = self.visit(node.expr)
+        body = self.visit_list(node.body)
+        node_uid = uuid.uuid4()
+        self.G.add_node(node_uid,label="Loop")
+        self.G.add_edge(node_uid,expr)
+        for n in body:
+            self.G.add_edge(node_uid,n)
+
+        return node_uid
+    
+    @visit.register
+    def _(self, node: ModelBreak):
+        node_uid = uuid.uuid4()
+        self.G.add_node(node_uid, label="Break")
+        return node_uid
+
+    @visit.register
+    def _(self, node: ModelContinue):
+        node_uid = uuid.uuid4()
+        self.G.add_node(node_uid, label="Continue")
+        return node_uid
 
     @visit.register
     def _(self, node: ModelIf):
@@ -137,4 +201,78 @@ class CASTToAGraphVisitor(CASTVisitor):
 
         return node_uid
 
-    
+    @visit.register
+    def _(self, node: ModelReturn):
+        value = self.visit(node.value)
+        node_uid = uuid.uuid4()
+        self.G.add_node(node_uid, label="Return")
+        self.G.add_edge(node_uid, value)
+
+        return node_uid
+
+    @visit.register
+    def _(self, node: Module):
+        return self.visit_list(node.body) 
+
+    @visit.register
+    def _(self, node: Name):
+        node_uid = uuid.uuid4()
+        self.G.add_node(node_uid,label=node.name)
+        return node_uid 
+
+    @visit.register
+    def _(self, node: Number):
+        node_uid = uuid.uuid4()
+        self.G.add_node(node_uid,label=node.number)
+        return node_uid 
+
+    @visit.register
+    def _(self, node: Set):
+        values = self.visit_list(node.values)
+        node_uid = uuid.uuid4()
+        self.G.add_node(node_uid,label="Set")
+        for n in values:
+            self.G.add_edge(node_uid,n)
+
+        return node_uid
+
+    @visit.register
+    def _(self. node: String):
+        node_uid = uuid.uuid4()
+        self.G.add_node(node_uid,label=node.string)
+        return node_uid 
+
+    @visit.register
+    def _(self, node: Subscript):
+        value = self.visit(node.value)
+        s_slice = self.visit(node.slice)
+        node_uid = uuid.uuid4()
+        self.G.add_node(node_uid, label="Subscript")
+        self.G.add_edge(node_uid, value)
+        self.G.add_edge(node_uid, s_slice)
+
+        return node_uid
+
+    @visit.register
+    def _(self, node: Tuple):
+        values = self.visit_list(node.values)
+        node_uid = uuid.uuid4()
+        self.G.add_node(node_uid,label="Tuple")
+        for n in values:
+            self.G.add_edge(node_uid,n)
+
+        return node_uid
+
+    @visit.register
+    def _(self, node: UnaryOp):
+        val = self.visit(node.value)
+        node_uid = uuid.uuid4()
+        self.G.add_node(node_uid, label=node.op)
+        self.G.add_edge(node_uid, val)
+
+        return node_uid        
+
+    @visit.register
+    def _(self, node: Var):
+        val = self.visit(node.val) 
+        return val
