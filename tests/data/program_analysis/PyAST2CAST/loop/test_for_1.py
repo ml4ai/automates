999dd311890fcf7e9f924fbd30a810b19d645256
titomeister <ferra@email.arizona.edu>
2021-03-31 11:03:59 -0700

    Cleaning up some files before testing

--- tests/data/program_analysis/PyAST2CAST/loop/test_for_1.py
+++
@@ -1,4 +0,0 @@
-# Basic for loop test
-def main():
-    for i in range(10):
-        pass
--- tests/data/program_analysis/PyAST2CAST/loop/test_while_1.py
+++
@@ -1,6 +0,0 @@
-# Basic while loop test
-def main():
-    i = 0
-    while(i < 10):
-        i = i + 1
-    
