f5e16ff92a420c113b66ed312d95cc3f328121d8
Clayton Morrison <claytonm@email.arizona.edu>
2021-06-13 17:24:51 -0700

    generator now creates ROOT_DATA path is not already exists

--- scripts/gromet/generate_examples.py
+++ scripts/gromet/generate_examples.py
@@ -7,16 +7,25 @@ import example_SimpleSIR_FN
 import example_SimpleSIR_PetriNetClassic
 import example_SimpleSIR_PrTNet
 import example_toy1
+import os
+import errno
 
 
 ROOT_DATA = 'examples'
 
 
-gromet_to_json(example_call_ex1.generate_gromet(), tgt_root=ROOT_DATA)
-gromet_to_json(example_cond_ex1.generate_gromet(), tgt_root=ROOT_DATA)
-gromet_to_json(example_emmaaSBML_PetriNetClassic.generate_gromet(), tgt_root=ROOT_DATA)
-gromet_to_json(example_SimpleSIR_Bilayer.generate_gromet(), tgt_root=ROOT_DATA)
-gromet_to_json(example_SimpleSIR_FN.generate_gromet(), tgt_root=ROOT_DATA)
-gromet_to_json(example_SimpleSIR_PetriNetClassic.generate_gromet(), tgt_root=ROOT_DATA)
-gromet_to_json(example_SimpleSIR_PrTNet.generate_gromet(), tgt_root=ROOT_DATA)
-gromet_to_json(example_toy1.generate_gromet(), tgt_root=ROOT_DATA)
+if __name__ == '__main__':
+    try:
+        os.makedirs(ROOT_DATA)
+        gromet_to_json(example_call_ex1.generate_gromet(), tgt_root=ROOT_DATA)
+        gromet_to_json(example_cond_ex1.generate_gromet(), tgt_root=ROOT_DATA)
+        gromet_to_json(example_emmaaSBML_PetriNetClassic.generate_gromet(), tgt_root=ROOT_DATA)
+        gromet_to_json(example_SimpleSIR_Bilayer.generate_gromet(), tgt_root=ROOT_DATA)
+        gromet_to_json(example_SimpleSIR_FN.generate_gromet(), tgt_root=ROOT_DATA)
+        gromet_to_json(example_SimpleSIR_PetriNetClassic.generate_gromet(), tgt_root=ROOT_DATA)
+        gromet_to_json(example_SimpleSIR_PrTNet.generate_gromet(), tgt_root=ROOT_DATA)
+        gromet_to_json(example_toy1.generate_gromet(), tgt_root=ROOT_DATA)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+
