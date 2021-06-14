5d9771c47775dbd21a642285f0b23f0139fe66d5
Clayton Morrison <claytonm@email.arizona.edu>
2021-06-13 18:23:04 -0700

    updated docstring and filled out Measure.interval

--- scripts/gromet/gromet_experiment.py
+++ scripts/gromet/gromet_experiment.py
@@ -1,4 +1,5 @@
 from gromet import *
+from typing import Any
 
 
 # -----------------------------------------------------------------------------
@@ -10,12 +11,12 @@ class Measure(GrometElm):
     """
     Defines a Measure.
     'source' references the UidPort or UidJunction whose value will be measured.
-    'interval':
+    'interval': either a list of time points or a tuple (<start>, <end>)
     'type':
     """
     measure_id: UidMeasure
     source: Union[UidPort, UidJunction]
-    interval: None  # TODO: Tuple or Array
+    interval: Union[List[float], Tuple[float, float]]
     type: UidType  # Enum("M:instance", "M:interval", "M:steady_state")
 
 
@@ -24,11 +25,13 @@ class ParameterAssignment(Valued):
     """
     Specifies the value for a parameter in the model.
     The 'source' identifies the Port or Junction that will be set.
-    This class extends Valued, so it has a value and value_type.
+    This class extends Valued, so it has a 'value' and 'value_type'.
     If 'value' is None:
-        use value already present in source as the parameter value.
+        execution framework will use value already present in source
+        as the (initial) parameter value.
     If 'value' is not None:
-        use this value as the parameter value.
+        execution framework will use the provided value Literal as
+        the (initial) parameter value.
 
     """
     source: Union[UidPort, UidJunction]
@@ -42,12 +45,12 @@ class Fold(GrometElm):
     Including Fold(s) in the args list permits building more complex
     Folds that build on the results of other Folds.
     A Fold could be specified that itself takes no arguments and has
-    an Expression just "generates" a sequence of numbers (a predefined
-    sequence).
+    an Expression that just "generates" a sequence of numbers (a
+    predefined sequence).
     Expression type is required for the expression because we need to
-    define the Ports that may be accessed in the Expr tree, but have
-    the order of those Ports correspond to the order of the Fold args
-    list.
+    define the Ports that may be accessed in the Expr tree;
+    the order of those Ports corresponds to the order of the Fold args
+    list, for mapping "inputs" to the Expression.
     """
     args: List[Union[UidMeasure, 'Fold']]
     expression: Expression
@@ -61,6 +64,9 @@ class Fold(GrometElm):
 
 @dataclass
 class ExperimentSpec(GrometElm, ABC):
+    """
+    Base Experiment Specification.
+    """
     measures: List[Measure]
     folds: List[Fold]
     parameters: List[ParameterAssignment]  # initial conditions
@@ -69,7 +75,8 @@ class ExperimentSpec(GrometElm, ABC):
 @dataclass
 class FrontendExperimentSpec(ExperimentSpec):
     """
-    Message sent by HMI to Proxy
+    Message sent by HMI to Proxy.
+    Only references model by it's GroMEt Uid.
     """
     model_id: UidGromet
 
@@ -77,16 +84,27 @@ class FrontendExperimentSpec(ExperimentSpec):
 @dataclass
 class BackendExperimentSpec(ExperimentSpec):
     """
-    Message sent from Proxy to Backend
+    Message sent from Proxy to Backend.
+    Includes GroMEt payload (assumes proxy retrieves GroMEt from store).
     """
     model: Gromet
 
 
 @dataclass
 class ExperimentSpecSet(GrometElm):
+    """
+    A collection of experiment specifications.
+    """
     experiments: List[ExperimentSpec]
 
 
 @dataclass
 class ExperimentResults(GrometElm):
+    """
+    Results of an experiment.
+    'measurements' is a collection of tuples of:
+        <Uid of Measure>, <sequence of values (anything)>
+    In the case of indexed return values, the sequence of values
+        will be tuples: (<index>, <value>)
+    """
     measurements: List[Tuple[UidMeasure, List[Any]]]
