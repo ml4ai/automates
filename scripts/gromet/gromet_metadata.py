fe9d192722fc0f8931fa2aaa11fd106bdb549719
Clayton Morrison <claytonm@email.arizona.edu>
2021-06-14 09:38:33 -0700

    more metadatums

--- scripts/gromet/gromet_metadata.py
+++ scripts/gromet/gromet_metadata.py
@@ -7,8 +7,12 @@ from dataclasses import dataclass, field
 # Uid
 # =============================================================================
 
+# This also gets defined in gromet.py, which imports this file...
+UidVariable = NewType('UidVariable', str)
+
 UidMetadatum = NewType('UidMetadatum', str)
 UidDocumentReference = NewType('UidDocumentReference', str)
+UidCodeFileReference = NewType('UidCodeFileReference', str)
 
 # ISO 8601 Extended format: YYYY-MM-DDTHH:mm:ss:sssZ
 # where
@@ -76,6 +80,76 @@ Metadata = NewType('Metadata', Union[List[Metadatum], None])
 
 
 # =============================================================================
+# Metadata components
+# =============================================================================
+
+@dataclass
+class TextExtraction:
+    """
+    Text extraction.
+    'document_reference_uid' should match the uid of a
+      TextualDocumentReference.
+    COSMOS within-document reference coordinates to the span of text.
+      'block' is found on a 'page'
+      'char_begin' and 'char_end' are relative to the 'block'.
+    """
+    document_reference_uid: UidDocumentReference
+    page: int
+    block: int
+    char_begin: int
+    char_end: int
+
+
+@dataclass
+class EquationExtraction:
+    """
+    'document_reference_uid' should match the uid of a
+      TextualDocumentReference.
+    'equation_number' is 0-indexed, relative order of equation
+      as identified in the document.
+    """
+    document_reference_uid: UidDocumentReference
+    equation_number: int
+    equation_source_latex: str  # latex
+    equation_source_mml: str    # MathML
+
+
+@dataclass
+class CodeFileReference:
+    """
+    'name': filename
+    'path': Assume starting from root of code collection
+    """
+    uid: UidCodeFileReference
+    name: str
+    path: str
+
+
+# =============================================================================
+# Metadata host: <Any>
+# metadata that may be associated with any GroMEt element
+# =============================================================================
+
+# -----------------------------------------------------------------------------
+# CodeSpanReference
+# -----------------------------------------------------------------------------
+
+@dataclass
+class CodeSpanReference(Metadatum):
+    """
+    'code_type': One of 'IDENTIFIER', 'CODE_BLOCK'
+    code span coordinates are relative to the source file
+        (denoted by the file_id)
+    """
+    code_type: str  # 'IDENTIFIER', 'CODE_BLOCK'
+    file_id: CodeFileReference
+    line_begin: int
+    line_end: int
+    col_begin: int
+    col_end: int
+
+
+# =============================================================================
 # Metadata host: <Gromet>
 # metadata associated with a top-level <Gromet> object
 # =============================================================================
@@ -105,13 +179,17 @@ class Bibjson:
 class TextualDocumentReference:
     """
     Reference to an individual document
+    'cosmos_id': ID of COSMOS component used to process document.
+    'cosmos_version_number': Version number of COSMOS component.
+    'automates_id': ID of AutoMATES component used to process document.
+    'automates_version_number': Version number of AutoMATES component.
     """
     uid: UidDocumentReference
     global_reference_id: GlobalReferenceId
-    cosmos_id: str  # ID of COSMOS component used to process document.
-    cosmos_version_number: str  # Version number of COSMOS component.
-    automates_id: str  # ID of AutoMATES component used to process document.
-    automates_version_number: str  # Version number of AutoMATES component.
+    cosmos_id: str
+    cosmos_version_number: str
+    automates_id: str
+    automates_version_number: str
     bibjson: Bibjson
 
 
@@ -125,13 +203,62 @@ class TextualDocumentReferenceSet(Metadatum):
     documents: List[TextualDocumentReference]
 
 
+# -----------------------------------------------------------------------------
+# CodeCollectionReference
+# -----------------------------------------------------------------------------
+
+@dataclass
+class CodeCollectionReference(Metadatum):
+    """
+    host: <gromet>
+    Reference to a code collection (i.e., repository)
+    """
+    global_reference_id: GlobalReferenceId
+    file_ids: List[CodeFileReference]
+
+
+# =============================================================================
+# Metadata host: <Box>
+# metadata associated with a Box
+# =============================================================================
+
+@dataclass
+class EquationDefinition(Metadatum):
+    """
+    host: <Box>
+    Association of an equation extraction with a Box
+        (e.g., Function, Expression, Relation).
+    """
+    equation_extraction: EquationExtraction
+
+
+# =============================================================================
+# Metadata host: <Variable>
+# metadata associated with a Variable
+# =============================================================================
+
+@dataclass
+class EquationParameter(Metadatum):
+    """
+    host: <Variable>
+    Association of parameter values extracted from equations.
+    """
+    equation_extraction: EquationExtraction
+    variable_uid: UidVariable
+
+
+# =============================================================================
 # =============================================================================
 # CHANGE LOG
+# =============================================================================
 # =============================================================================
 
 """
 Changes 2021-06-13:
 () Started migration of GrFN metadata types to GroMEt metadatum types.
+    () <Any>.CodeSpanReference
     () <Gromet>.TextualDocumentReferenceSet
-    () 
+    () <Gromet>.CodeCollectionReference
+    () <Box>.EquationDefinition
+    () <Variable>.EquationParameter
 """
