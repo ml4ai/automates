3e3b5377b2a90c7e3cd20ba56b6a9f4ebe0119e3
Clayton Morrison <claytonm@email.arizona.edu>
2021-06-19 18:44:46 -0700

    updated to add generation of examples with metadata

--- scripts/gromet/generate_examples.py
+++ scripts/gromet/generate_examples.py
@@ -3,8 +3,11 @@ import example_call_ex1
 import example_cond_ex1
 import example_emmaaSBML_PetriNetClassic
 import example_SimpleSIR_Bilayer
+import example_SimpleSIR_Bilayer_metadata
 import example_SimpleSIR_FN
+import example_SimpleSIR_FN_metadata
 import example_SimpleSIR_PetriNetClassic
+import example_SimpleSIR_PetriNetClassic_metadata
 import example_SimpleSIR_PrTNet
 import example_toy1
 import os
@@ -21,8 +24,11 @@ if __name__ == '__main__':
         gromet_to_json(example_cond_ex1.generate_gromet(), tgt_root=ROOT_DATA)
         gromet_to_json(example_emmaaSBML_PetriNetClassic.generate_gromet(), tgt_root=ROOT_DATA)
         gromet_to_json(example_SimpleSIR_Bilayer.generate_gromet(), tgt_root=ROOT_DATA)
+        gromet_to_json(example_SimpleSIR_Bilayer_metadata.generate_gromet(), tgt_root=ROOT_DATA)
         gromet_to_json(example_SimpleSIR_FN.generate_gromet(), tgt_root=ROOT_DATA)
+        gromet_to_json(example_SimpleSIR_FN_metadata.generate_gromet(), tgt_root=ROOT_DATA)
         gromet_to_json(example_SimpleSIR_PetriNetClassic.generate_gromet(), tgt_root=ROOT_DATA)
+        gromet_to_json(example_SimpleSIR_PetriNetClassic_metadata.generate_gromet(), tgt_root=ROOT_DATA)
         gromet_to_json(example_SimpleSIR_PrTNet.generate_gromet(), tgt_root=ROOT_DATA)
         gromet_to_json(example_toy1.generate_gromet(), tgt_root=ROOT_DATA)
     except OSError as e:
