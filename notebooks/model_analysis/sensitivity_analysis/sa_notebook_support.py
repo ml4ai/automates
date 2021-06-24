b6d445fd35f5ee275f53a94d294b4d8854adaaa1
aswinchester <winchester@email.arizona.edu>
2021-06-23 21:25:16 -0700

    fixed small issue with epic table

---
+++ notebooks/model_analysis/sensitivity_analysis/sa_notebook_support.py
@@ -0,0 +1,14 @@
+import json
+import os
+import csv
+import math
+import numpy as np
+import pandas as pd
+
+from automates.model_assembly.networks import GroundedFunctionNetwork
+from automates.model_analysis.sensitivity import (
+    SensitivityAnalyzer,
+    SADependentVariable,
+)
+
+def soilt_sa
