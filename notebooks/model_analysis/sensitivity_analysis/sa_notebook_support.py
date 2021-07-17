552bf684d6e3def40e71cf01396185702045f9bf
aswinchester <winchester@email.arizona.edu>
2021-07-17 14:18:41 -0500

    SA notebooks/script updated to make S2 indices optional

--- notebooks/model_analysis/sensitivity_analysis/sa_notebook_support.py
+++ notebooks/model_analysis/sensitivity_analysis/sa_notebook_support.py
@@ -11,7 +11,7 @@ from automates.model_analysis.sensitivity import (
     SADependentVariable,
 )
 
-def run_sa(file, model_args, N):
+def run_sa(file, model_args, N, compute_S2):
     print("Reading soilt json file...")
     if not os.path.exists(file):
         raise Exception(f"Error: File '{file}' does not exist")
@@ -26,7 +26,7 @@ def run_sa(file, model_args, N):
 
     print("Running sensitivity analysis...")
     analyzer = SensitivityAnalyzer()
-    Si_list = analyzer.Si_from_Sobol(N, grfn, bounds, inputs, outputs)
+    Si_list = analyzer.Si_from_Sobol(N, grfn, bounds, inputs, outputs, calc_2nd=compute_S2)
     model_inputs = [
         {
             "variable_name": k,
@@ -44,13 +44,19 @@ def run_sa(file, model_args, N):
     return results
 
 
-def to_table(sa_obj):
-    #     json_file = file
+def to_table(csv_table):
+    old_table = np.array(csv_table)
+    col_names = old_table[0, :]
+    row_names = old_table[:, 0]
 
-    #     sa_obj = {}
-    #     with open(json_file) as f:
-    #         sa_obj = json.load(f)
+    new_table = old_table[1:len(row_names), 1:len(col_names)]
+    col_names = np.delete(col_names, 0)
+    row_names = np.delete(row_names, 0)
 
+    return pd.DataFrame(new_table, columns=col_names, index=row_names)
+
+
+def pd_tables(sa_obj, compute_S2=True):
     outputs = []
     inputs = []
 
@@ -77,19 +83,49 @@ def to_table(sa_obj):
             str(si['S1_conf'][i]).split(".")[0] + "." + str(si['S1_conf'][i]).split(".")[1][:3]
 
             table[idx + 1][i + 1] = f"{S1_val} +/- {S1_conf}"
-    #     return table
 
-    #     csv_file_name = f"{json_file.rsplit('.', 1)[0]}.csv"
-    #     with open(csv_file_name, "w") as csv_file:
-    #         csvWriter = csv.writer(csv_file, delimiter=',')
-    #         csvWriter.writerows(table)
+    # ST Table
+    st_table = [[""] * (len(inputs) + 1) for v in range(len(outputs) + 1)]
 
-    old_table = np.array(table)
-    col_names = old_table[0, :]
-    row_names = old_table[:, 0]
+    for col in range(len(inputs)):
+        st_table[0][col + 1] = inputs[col]
 
-    new_table = old_table[1:len(row_names), 1:len(col_names)]
-    col_names = np.delete(col_names, 0)
-    row_names = np.delete(row_names, 0)
+    for idx, _ in enumerate(outputs):
+        si = sa_obj['sensitivity_indices'][idx]
+        st_table[idx + 1][0] = outputs[idx]
+        for i in range(len(si['ST'])):
+            ST_val = "--" if si['ST'][i] is None or math.isnan(si['ST'][i]) else str(si['ST'][i]).split(".")[0] + "." + \
+                                                                                 str(si['ST'][i]).split(".")[1][:3]
+            ST_conf = "--" if si['ST_conf'][i] is None or math.isnan(si['ST_conf'][i]) else \
+            str(si['ST_conf'][i]).split(".")[0] + "." + str(si['ST_conf'][i]).split(".")[1][:3]
 
-    return pd.DataFrame(new_table, columns=col_names, index=row_names)
+            st_table[idx + 1][i + 1] = f"{ST_val} +/- {ST_conf}"
+
+    # S2 Table
+    if compute_S2:
+        n_inputs = len(inputs)
+        n_cols = math.comb(n_inputs, 2)
+        s2_table = [[""] * (n_cols + 1) for v in range(len(outputs) + 1)]
+        s2_columns = [""]
+        for i in range(len(inputs)):
+            for j in range(i + 1, n_inputs):
+                s2_columns.append(f"{inputs[i]}, {inputs[j]}")
+        s2_table[0] = s2_columns
+
+        for idx, _ in enumerate(outputs):
+            si = sa_obj['sensitivity_indices'][idx]
+            s2_raw = si['S2']
+            s2_raw_conf = si['S2_conf']
+            s2_trimmed = [outputs[idx]]
+            for i in range(n_inputs):
+                for j in range(i + 1, n_inputs):
+                    s2_val = "--" if s2_raw[i][j] is None or math.isnan(s2_raw[i][j]) else str(s2_raw[i][j]).split(".")[
+                                                                                               0] + "." + \
+                                                                                           str(s2_raw[i][j]).split(".")[
+                                                                                               1][:3]
+                    s2_conf = "--" if s2_raw_conf[i][j] is None or math.isnan(s2_raw_conf[i][j]) else \
+                    str(s2_raw_conf[i][j]).split(".")[0] + "." + str(s2_raw_conf[i][j]).split(".")[1][:3]
+                    s2_trimmed.append(f"{s2_val} +/- {s2_conf}")
+            s2_table[idx + 1] = s2_trimmed
+        return (to_table(table), to_table(s2_table), to_table(st_table))
+    return (to_table(table), to_table(st_table))
