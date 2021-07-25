14b925bb585ae50e1577470c64bcacfffc36dd6c
Maria Alexeeva <maxaalexeeva@gmail.com>
2021-07-24 20:16:30 -0600

    found by is only for context attachments

--- automates/text_reading/src/main/scala/org/clulab/aske/automates/serializer/AutomatesSerializer.scala
+++ automates/text_reading/src/main/scala/org/clulab/aske/automates/serializer/AutomatesSerializer.scala
@@ -151,7 +151,6 @@ object AutomatesJSONSerializer {
 
   def toAttachment(json: ujson.Value): Attachment = {
     val attType = json("attType").str
-    val foundBy = json("foundBy").str
     val toReturn = attType match {
       case "MentionLocation" => new MentionLocationAttachment(json("pageNum").num.toInt, json("blockIdx").num.toInt, attType)
       case "DiscontinuousCharOffset" => new DiscontinuousCharOffsetAttachment(json("charOffsets").arr.map(v => (v.arr.head.num.toInt, v.arr.last.num.toInt)), attType)
@@ -166,7 +165,10 @@ object AutomatesJSONSerializer {
       }
 
       case "UnitAtt" => new UnitAttachment(json("attachedTo").str, attType)
-      case "ContextAtt" => new ContextAttachment(attType, json("contexts").arr, foundBy)
+      case "ContextAtt" => {
+        val foundBy = json("foundBy").str
+        new ContextAttachment(attType, json("contexts").arr, foundBy)
+      }
       case _ => ???
     }
     toReturn
