d16c4dc09e6362c1e9705cdb0552ad8a79bb0741
Maria Alexeeva <maxaalexeeva@gmail.com>
2021-08-07 16:04:00 -0600

    making subclass of optional; only search in label; added links to resources

--- automates/text_reading/sparql/sparqlWikiWrapper.py
+++ automates/text_reading/sparql/sparqlWikiWrapper.py
@@ -2,6 +2,8 @@ import requests
 import sys
 term = sys.argv[1]
 
+# tutorial: https://www.wikidata.org/wiki/Wikidata:SPARQL_tutorial
+#" It is possible for a query to access Wikidata's search function with the MediaWiki API Query Service. [...]  uses the MediaWiki API, and can therefore run without timeout" (https://www.wikidata.org/wiki/Wikidata:SPARQL_query_service/query_optimization)
 #the link to the query on wikidata api https://w.wiki/Sfu
 #todo for the query:
 #have this regex as filter: "^(\\w*\\s){{0,2}}{term}(\\sof)?(\\s\\w*){{0,3}}$"
@@ -19,23 +21,24 @@ WHERE
     bd:serviceParam wikibase:endpoint "www.wikidata.org";
 wikibase:api "Generator";
 mwapi:generator "search";
-mwapi:gsrsearch "'{term}'";
+mwapi:gsrsearch "inlabel:'{term}'";
 mwapi:gsrlimit "max".
 ?item wikibase:apiOutputItem mwapi:title.
 }}
 ?item rdfs:label ?label.
+OPTIONAL {{ ?item wdt:P279 ?subclassOf. }}
 ?sitelink ^schema:name ?article .
 ?article schema:about ?item ;
     schema:isPartOf <https://en.wikipedia.org/> .
 
-    ?item wdt:P279 ?subclassOf.
+
     FILTER REGEX (?label, "^(\\\\w*\\\\s){{0,2}}?{term}(\\\\sof)?(\\\\s\\\\w*){{0,3}}?$")
 
     SERVICE wikibase:label {{ bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }}
 }}
 order by strlen(str(?label))
 
-LIMIT 30
+LIMIT 20
 """
 r = requests.get(url, params = {'format': 'json', 'query': query})
 data = r.json()
@@ -50,7 +53,7 @@ for result in data["results"]["bindings"]:
     subClassOf = result['subclassOf']['value'] if 'subclassOf' in result.keys()  else "NA"
 
     print(f"{term}\t{item}\t{itemLabel}\t{descr}\t{altLabel}\t{subClassOf}")
-	
+
 
 #the link to the query on wikidata api https://w.wiki/Sfu
 
