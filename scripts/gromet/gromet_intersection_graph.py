f05c9a96652e4509f5155447e8a5f9faebc7b1d7
Clayton Morrison <claytonm@email.arizona.edu>
2021-08-11 06:42:16 -0700

    added documentation

--- scripts/gromet/gromet_intersection_graph.py
+++ scripts/gromet/gromet_intersection_graph.py
@@ -12,6 +12,14 @@ UidVariable = NewType('UidVariable', str)
 
 @dataclass
 class GrometIds:
+    """
+    Identify the two source GroMEt representations that will
+    be compared. Source GroMEts are identified by their uid.
+    Within this JSON format, fields representing content
+    corresponding to the first model will be noted with filed
+    names that have the prefix 'g1_', and similarly the second
+    model with have field prefixed by 'g2_'.
+    """
     g1_name: str
     g1_uid: UidGromet
     g2_name: str
@@ -20,14 +28,38 @@ class GrometIds:
 
 @dataclass
 class CommonNode:
+    """
+    Nodes in the intersection graph representing variables
+    from the source GroMEt that are considered the same.
+
+    g1_variable and g2_variable are Lists (JSON arrays) in
+    order to permit multiple variables along a path in either
+    model being treated as identical (this happens when there
+    is a path of variables within a model where the value
+    value of the data along the path does not change).
+    """
     uid: UidCommonNode
-    type: str  # one of: 'input', 'output', 'internal'
+    # type: str  # one of: 'input', 'output', 'internal'
     g1_variable: List[UidVariable]
     g2_variable: List[UidVariable]
 
 
 @dataclass
 class OAPNode:
+    """
+    Overlapping Abstract Path Node.
+    This node is an abstraction indicating that a collection
+    of variables (and the operations along paths between those
+    variables that modify the values, as represented by
+    Expressions, Functions, Loops, etc...) from either model
+    are not the same, but CommonNodes (variables from either
+    model that are identified as matching) serve as inputs
+    to and outputs from these paths. That is, although the
+    details of what is computed along paths from either model
+    may be different, there are overlapping/matching variables
+    from either model that play the same role as input to and
+    output from these paths.
+    """
     uid: UidCommonNode
     g1_variables: Union[List[UidVariable], None]
     g2_variables: Union[List[UidVariable], None]
@@ -35,6 +67,14 @@ class OAPNode:
 
 @dataclass
 class NOAPNode:
+    """
+    Non-Overlapping Abstract Path Node.
+    This node represents a collection of variables (and therefore
+    all of the paths between these variables) from a single model,
+    for which dataflow may go *in* to and/or *out* from, but this
+    collection of paths does not have corresponding paths in the
+    other model.
+    """
     uid: UidCommonNode
     gromet_name: str
     variables: List[UidVariable]
@@ -42,6 +82,10 @@ class NOAPNode:
 
 @dataclass
 class Edge:
+    """
+    Represent paths of dataflow from one CommonNode, OAPNode or
+    NOAPNode to another.
+    """
     type: str  # one of: 'equal', 'g1_subset_g2', 'g2_subset_g1', 'no_overlap'
     src: UidCommonNode
     dst: UidCommonNode
@@ -49,6 +93,10 @@ class Edge:
 
 @dataclass
 class GrometIntersectionGraph:
+    """
+    The top-level JSON object representing the Gromet Intersection
+    Graph.
+    """
     uid: UidIntersectionGraph
     gromet_ids: GrometIds
 
