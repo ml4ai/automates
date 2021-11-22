d144a1c08bb38a929ed834262b37c2b81b143963
Maria Alexeeva <maxaalexeeva@gmail.com>
2021-11-22 11:01:58 -0700

    combining mentions from all files

--- automates/text_reading/src/main/scala/org/clulab/aske/automates/apps/ExtractAndAssembleMentionEvents.scala
+++ automates/text_reading/src/main/scala/org/clulab/aske/automates/apps/ExtractAndAssembleMentionEvents.scala
@@ -1,20 +1,15 @@
 package org.clulab.aske.automates.apps
 
-import java.io.{BufferedWriter, File, FileWriter, PrintWriter}
+import java.io.{File, PrintWriter}
 import ai.lum.common.ConfigUtils._
 import com.typesafe.config.{Config, ConfigFactory}
 import org.clulab.aske.automates.data.{CosmosJsonDataLoader, DataLoader, TextRouter}
 import org.clulab.aske.automates.OdinEngine
 import org.clulab.aske.automates.apps.ExtractAndAlign.{getGlobalVars, returnAttachmentOfAGivenTypeOption}
-import org.clulab.aske.automates.attachments.{AutomatesAttachment, MentionLocationAttachment}
-import org.clulab.aske.automates.cosmosjson.CosmosJsonProcessor
+import org.clulab.aske.automates.attachments.MentionLocationAttachment
 import org.clulab.aske.automates.mentions.CrossSentenceEventMention
-import org.clulab.aske.automates.serializer.AutomatesJSONSerializer
 import org.clulab.utils.{FileUtils, Serializer}
 import org.clulab.odin.{EventMention, Mention}
-import org.clulab.odin.serialization.json.JSONSerializer
-import org.clulab.utils.AlignmentJsonUtils.GlobalVariable
-import org.json4s.jackson.JsonMethods._
 
 import scala.collection.mutable.ArrayBuffer
 
@@ -39,7 +34,7 @@ object ExtractAndAssembleMentionEvents extends App {
 
   val numOfWikiGroundings: Int = config[Int]("apps.numOfWikiGroundings")
   val inputDir: String = config[String]("apps.inputDirectory")
-  val outputDir: String = config[String]("apps.outputDirectory")
+  val outputDir: String = "/Users/alexeeva/Desktop/automates-related/MentionAssemblyDebug/mentions/"//config[String]("apps.outputDirectory")
   val inputType: String = config[String]("apps.inputType")
   val dataLoader = DataLoader.selectLoader(inputType) // pdf, txt or json are supported, and we assume json == cosmos json; to use science parse. comment out this line and uncomment the next one
   //  val dataLoader = new ScienceParsedDataLoader
@@ -52,6 +47,93 @@ object ExtractAndAssembleMentionEvents extends App {
   //  val commentReader = OdinEngine.fromConfig(config[Config]("CommentEngine"))
   //  val textRouter = new TextRouter(Map(TextRouter.TEXT_ENGINE -> reader, TextRouter.COMMENT_ENGINE -> commentReader))
   // For each file in the input directory:
+
+
+
+  def getMentionsWithoutLocations(texts: Seq[String], file: File): Seq[Mention] = {
+    // this is for science parse
+    texts.flatMap(t => reader.extractFromText(t, filename = Some(file.getName)))
+  }
+
+  def getMentionsWithLocations(texts: Seq[String], file: File): Seq[Mention] = {
+    // this is for cosmos jsons
+    val textsAndFilenames = texts.map(_.split("<::>").slice(0,2).mkString("<::>"))
+    val locations = texts.map(_.split("<::>").takeRight(2).mkString("<::>")) //location = pageNum::blockIdx
+    val mentions = for (tf <- textsAndFilenames) yield {
+      val Array(text, filename) = tf.split("<::>")
+      reader.extractFromText(text, keepText = true, Some(filename))
+    }
+    // store location information from cosmos as an attachment for each mention
+    val menWInd = mentions.zipWithIndex
+    val mentionsWithLocations = new ArrayBuffer[Mention]()
+    for (tuple <- menWInd) {
+      // get page and block index for each block; cosmos location information will be the same for all the mentions within one block
+      val menInTextBlocks = tuple._1
+      val id = tuple._2
+      val location = locations(id).split("<::>").map(loc => loc.split(",").map(_.toInt)) //(_.toDouble.toInt)
+      val pageNum = location.head
+      val blockIdx = location.last
+
+      for (m <- menInTextBlocks) {
+        val newAttachment = new MentionLocationAttachment(file.getName, pageNum, blockIdx, "MentionLocation")
+        val newMen = m match {
+          case m: CrossSentenceEventMention => m.asInstanceOf[CrossSentenceEventMention].newWithAttachment(newAttachment)
+          case _ => m.withAttachment(newAttachment)
+        }
+        mentionsWithLocations.append(newMen)
+      }
+    }
+    mentionsWithLocations
+  }
+
+  val textMentions = new ArrayBuffer[Mention]()
+  files.par.foreach { file =>
+    // 1. Open corresponding output file and make all desired exporters
+    println(s"Extracting from ${file.getName}")
+    // 2. Get the input file contents
+    // note: for science parse format, each text is a section
+    val texts = dataLoader.loadFile(file)
+
+
+    // todo: make a json with things like "models": [{name: Blah, descr: Blah}], countries: {}, dates: {can I also get the event here? maybe with my new found previously found mention as a trigger power?}, params: {vars from units, param settings, and descriptions}, model_info: {model descr, model limitation, etc}, param settings and units --- method to combine units and param setting based on var overlap
+
+//    obj("file_name") = file.getName
+    // 3. Extract causal mentions from the texts
+    val mentions = if (file.getName.contains("COSMOS")) {
+      // cosmos json
+      getMentionsWithLocations(texts, file)
+    } else {
+      // other file types---those don't have locations
+      getMentionsWithoutLocations(texts, file)
+    }
+
+
+    textMentions.appendAll(mentions)
+  }
+
+  val obj = assembleMentions(textMentions, Seq.empty)
+
+  println("OBJECT: " + obj)
+
+
+  writeJsonToFile(obj, outputDir, "assembledMentions-Nov22.json")
+
+
+
+
+  val labels = textMentions.map(_.label).distinct.mkString("||")
+  println("Labels: " + labels)
+
+  def writeJsonToFile(obj: ujson.Value, outputDir: String, outputFileName: String): Unit = {
+
+    val json = ujson.write(obj, indent = 2)
+
+    //      val pw = new PrintWriter(new File(outputDir + file.getName.replace(".json", "-assembled-mentions.json")))
+    val pw = new PrintWriter(new File(outputDir + outputFileName))
+    pw.write(json)
+    pw.close()
+
+  }
 
   def assembleMentions(textMentions: Seq[Mention], mdMentions: Seq[Mention]): ujson.Value = {
     val obj = ujson.Obj()
@@ -84,7 +166,7 @@ object ExtractAndAssembleMentionEvents extends App {
                 sourceJson("blockIdx") = sourceAsJson("blockIdx")
                 sourceJson("filename") = sourceAsJson("filename")
               }
-//              sourceJson("filename") = filename
+              //              sourceJson("filename") = filename
               sourceJson
             }
             val source = getSource(m)
@@ -205,87 +287,5 @@ object ExtractAndAssembleMentionEvents extends App {
       }
     }
     obj
-  }
-
-  def getMentionsWithoutLocations(texts: Seq[String], file: File): Seq[Mention] = {
-    // this is for science parse
-    texts.flatMap(t => reader.extractFromText(t, filename = Some(file.getName)))
-  }
-
-  def getMentionsWithLocations(texts: Seq[String], file: File): Seq[Mention] = {
-    // this is for cosmos jsons
-    val textsAndFilenames = texts.map(_.split("<::>").slice(0,2).mkString("<::>"))
-    val locations = texts.map(_.split("<::>").takeRight(2).mkString("<::>")) //location = pageNum::blockIdx
-    val mentions = for (tf <- textsAndFilenames) yield {
-      val Array(text, filename) = tf.split("<::>")
-      reader.extractFromText(text, keepText = true, Some(filename))
-    }
-    // store location information from cosmos as an attachment for each mention
-    val menWInd = mentions.zipWithIndex
-    val mentionsWithLocations = new ArrayBuffer[Mention]()
-    for (tuple <- menWInd) {
-      // get page and block index for each block; cosmos location information will be the same for all the mentions within one block
-      val menInTextBlocks = tuple._1
-      val id = tuple._2
-      val location = locations(id).split("<::>").map(loc => loc.split(",").map(_.toInt)) //(_.toDouble.toInt)
-      val pageNum = location.head
-      val blockIdx = location.last
-
-      for (m <- menInTextBlocks) {
-        val newAttachment = new MentionLocationAttachment(file.getName, pageNum, blockIdx, "MentionLocation")
-        val newMen = m match {
-          case m: CrossSentenceEventMention => m.asInstanceOf[CrossSentenceEventMention].newWithAttachment(newAttachment)
-          case _ => m.withAttachment(newAttachment)
-        }
-        mentionsWithLocations.append(newMen)
-      }
-    }
-    mentionsWithLocations
-  }
-
-  files.par.foreach { file =>
-    // 1. Open corresponding output file and make all desired exporters
-    println(s"Extracting from ${file.getName}")
-    // 2. Get the input file contents
-    // note: for science parse format, each text is a section
-    val texts = dataLoader.loadFile(file)
-
-
-    // todo: make a json with things like "models": [{name: Blah, descr: Blah}], countries: {}, dates: {can I also get the event here? maybe with my new found previously found mention as a trigger power?}, params: {vars from units, param settings, and descriptions}, model_info: {model descr, model limitation, etc}, param settings and units --- method to combine units and param setting based on var overlap
-
-//    obj("file_name") = file.getName
-    // 3. Extract causal mentions from the texts
-    val mentions = if (file.getName.contains("COSMOS")) {
-      // cosmos json
-      getMentionsWithLocations(texts, file)
-    } else {
-      // other file types---those don't have locations
-      getMentionsWithoutLocations(texts, file)
-    }
-
-    val obj = assembleMentions(mentions, Seq.empty)
-
-    println("OBJECT: " + obj)
-
-    val outputDir = "/Users/alexeeva/Desktop/automates-related/MentionAssemblyDebug/mentions/"
-    writeJsonToFile(obj, outputDir, "assembledMentions-Nov22.json")
-
-
-
-
-    val labels = mentions.map(_.label).distinct.mkString("||")
-    println("Labels: " + labels)
-
-  }
-
-  def writeJsonToFile(obj: ujson.Value, outputDir: String, outputFileName: String): Unit = {
-
-    val json = ujson.write(obj, indent = 2)
-
-    //      val pw = new PrintWriter(new File(outputDir + file.getName.replace(".json", "-assembled-mentions.json")))
-    val pw = new PrintWriter(new File(outputDir + outputFileName))
-    pw.write(json)
-    pw.close()
-
   }
 }
