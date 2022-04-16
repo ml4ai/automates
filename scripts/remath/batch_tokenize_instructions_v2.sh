63e7de255588a4df849679fbe53a6aa547159d0e
kcdharma <ck.amrahd@gmail.com>
2022-04-16 10:22:17 -0700

    batch tokenization v2

---
+++ scripts/remath/batch_tokenize_instructions_v2.sh
@@ -0,0 +1,5 @@
+# sample file to run the batch_tokenize_instructions_v2.py
+
+python batch_tokenize_instructions_v2.py -I expr_v2_perfect/expr_v2_0236810-large \
+-i expr_v2_0236810__Linux-5.4.0-81-generic-x86_64-with-glibc2.31__gcc-10.1.0-instructions.txt \
+-D expr_v2_perfect/output
