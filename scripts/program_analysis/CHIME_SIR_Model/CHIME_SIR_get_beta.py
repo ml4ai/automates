b5faf2a10dbbad1f56dc39a2f1b39d3f1c95baa8
titomeister <ferra@email.arizona.edu>
2022-08-16 09:44:15 -0700

    Cleaning up CHIME_SIR files that don't need to be up on the remote branch

--- scripts/program_analysis/CHIME_SIR_Model/CHIME_SIR_get_beta.py
+++
@@ -1,7 +0,0 @@
-def get_beta(intrinsic_growth_rate, gamma,           
-             susceptible, relative_contact_rate):    
-    inv_contact_rate = 1.0 - relative_contact_rate  
-    updated_growth_rate = intrinsic_growth_rate + gamma  
-    beta = updated_growth_rate / susceptible * inv_contact_rate 
- 
-    return beta  
--- scripts/program_analysis/CHIME_SIR_Model/inline_func.py
+++
@@ -1,4 +0,0 @@
-def foo(x):
-    y = x + 1
-    def bar(z):
-        w = z + 2
